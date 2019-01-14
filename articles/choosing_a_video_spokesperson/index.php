28eaf2461b6fc5dd854a0f5201bfb34878522e64
Andy Knudsen <andy@websitetalkingheads.com>
2019-01-13 22:27:51 -0700

    choosing a spokesperson

--- articles/choosing_a_video_spokesperson/index.php
+++ articles/choosing_a_video_spokesperson/index.php
@@ -16,11 +16,15 @@
 	<meta name="verify-v1" content="YNESpqoAwK51PmBV7/PFKLG0agx7AQPKhXXcYAXGGF8="/>
 	<meta name="norton-safeweb-site-verification" content="iinbv24r-1ix20hgj5l94wz2rnn3aiwi0336hwysvvpiskquy6ijsh9wy12f3znbed-hz1ay8ppzhgqap-sicqtw6ui29d0wrfcpenudh1ml9xwjbej7u25xy9pnm6yr"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<?php include("../includes/head-b4.php"); ?>
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+	<link href="../../css/bootstrap.css" rel="stylesheet" type="text/css">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </head>
 
 <body>
-	<?php include ('../includes/header_b4.php'); ?>
+	<?php include ('../../includes/header.php'); ?>
 	<section class="page-header text-center">
 		<h1>Choose the Best Website Spokesperson for your Business</h1>
 		<h2>Finding the Virtual Spokesperson that is right for you.</h2>
@@ -34,7 +38,7 @@
 					<p class="summary">Think of them like the presenters so you see on TV. When they're relaying info, news updates, messages or selling you a innovative product, their job may be to hold your attention and convince visitors to give what they're offering a go. Not an easy feat along with the short attention span that a lot of people have! This is among the most reasons why selecting the best online spokesperson for your website is fundamental.</p>
 				</div>
 				<div class="col-md-5">
-					<?php $video="Massive Wall Of Spokespeople";$alt="Just some of our professional video spokespeople" ;include("../ivideo/showInline.php"); ?>
+					<?php $video="Massive Wall Of Spokespeople";$alt="Just some of our professional video spokespeople" ;include("../../inculdes/showInline.php"); ?>
 				</div>
 			</div>
 		</div>
@@ -46,7 +50,7 @@
 					<h3 class="card-title text-uppercase">energy</h3>
 				</div>
 				<div class="card-body">
-					<?php $actor="Angie";$pull="right"; include("../ivideo/showActor.php"); ?>
+					<?php $actor="Angie";$pull="right"; include("../../ivideo/showActor.php"); ?>
 					<p>Even without saying a word, a person's energy can speak volumes. If someone is feeling off or uncomfortable, the way they speak and move would convey this. The same can be said, of course, for a website spokesperson. As such, always look for someone who has great energy that also transfers well into video; truth be told, some people can get TOO energetic and this may come off as overacting to the viewer. </p>
 					<p>Remember, enthusiasm cannot be faked. You can write a great script for your actor, but if they can't provide the right amount of energy for the job, even that would fall flat. </p>
 				</div>
@@ -56,7 +60,7 @@
 					<h3 class="card-title text-uppercase">VOcal CLARITY</h3>
 				</div>
 				<div class="card-body">
-					<?php $actor="Jed";$pull="right"; include("../ivideo/showActor.php"); ?>
+					<?php $actor="Jed";$pull="right"; include("../../ivideo/showActor.php"); ?>
 					<p>As they are speaking "directly" to your clients, your chosen video spokesperson should be able to deliver the message clearly. Now, it is important to note that clarity does not equal volume. It's in how the words are pronounced and intonated; if you have to listen two or three times to what the person is saying, then it's high time that you look for another actor for the job. </p>
 					<p>That said, always allow your spokesperson a bit of time to practice before you begin shooting. Allow them to warm-up to the script you have written and make notes on what must be emphasized. Choose from video spokespeople.</p>
 				</div>
@@ -66,7 +70,7 @@
 					<h3 class="card-title text-uppercase">AESTHETICS</h3>
 				</div>
 				<div class="card-body">
-					<?php $actor="Marissa";$pull="right"; include("../ivideo/showActor.php"); ?>
+					<?php $actor="Marissa";$pull="right"; include("../../ivideo/showActor.php"); ?>
 					<p>This is completely dependent on the type of image you want to be associated with your business. In other words a brand spokesperson.  For example, if you're recording your spokespeople for a business that deals with surfboards, you would want an actor who matches that image. They need to be relatable to your target market. Can you imagine a man dressed in a sleek suit trying to sell surfboards? </p>
 					<p>Diversity is another key component if you're aiming for relatability. Here's a fact: if a person can see themselves your chosen representative, they are likelier to trust your business more and be more willing to purchase what you're offering.</p>
 				</div>
@@ -84,16 +88,16 @@
 					<p class="summary text-uppercase">good luck!</p>
 				</div>
 				<div class="col-md-6">
-					<?php $video="Our Spokespeople";$alt="Many of our professional video spokespeople" ;include("../ivideo/showInline.php"); ?>
+					<?php $video="Our Spokespeople";$alt="Many of our professional video spokespeople" ;include("../../includes/showInline.php"); ?>
 				</div>
 			</div>
 		</div>
 	</section>
 
-	<?php include ('../includes/footer_b4.php'); ?>
-	<?php include ('../ivideo/modal-spokesperson.php'); ?>
+	<?php include ('../../includes/footer.php'); ?>
+	<?php include ('../../includes/modal-spokesperson.php'); ?>
 	<script src="https://vjs.zencdn.net/7.2.4/video.js"></script>
-	<script src="../js/iVideo-spokesperson.js"></script>
+	<script src="../../js/iVideo-spokesperson.js"></script>
 </body>
 
 </html>
