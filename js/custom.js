58ca898611343047633c2af509fcb0fb1ad79e8f
Andy Knudsen <paris_giovanni@yahoo.com>
2019-02-07 11:49:45 -0700

    test

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,7 @@
 // JavaScript Document
 $(document).ready(function () {
 	"use strict";
-	$('#headerHolder').load('http://www.talkingheads.video/includes/header.html');
-	$('#quote').load('http://www.talkingheads.video/includes/Request-Quote.php');
-	$('#footerHolder').load('http://www.talkingheads.video/includes/footer.html', function () {
+	$('#changeQuote').text(function () {
 		var footQuote = [
 			"We learned a long time ago that sometimes it's what you DON'T say that's even more important and really persuades the viewer to take action.",
 			"Awesome videos that get RESULTS are captivating, specific and tell YOUR story.  Not ANY story...YOUR story!", "Our videos keep brains constantly guessing and so they HAVE TO WATCH your video longer.", "We believe your story is different than your competitor's, don't you?", "We know all companies are not alike, so we work with you to create the best solution for your company.", "The creative process is not found in a template or premade images.  And a questionnaire Asks the wrong questions. Conversations and collaboration are needed to get the best Video.", "Our Videos are producing results that are astounding business owners.  They keep commming back for more.", "When can we show you how our process can transform your video strategy, engage your leads, and turn them into loyal clients.", "We are not your average studio, we don't do questionnaires or templates, we make YOUR video and fair prices, not Ad Agency prices.", "We have made tens of thousands of Videos for our very happy customers.", "We are continuing to work with great clients from around the North America and the world and we are having a great time brainstorming, collaborating and creating.", "Who knows what the best message is for your video?  I certainly don't know right now.  How could I? How could anyone? Until we really talk, ask questions, and listen to your answers.", "When you call us, we do the work for you.", "Really smart business owners know that it usually takes an outside person with experience in persuasion to know what should be left out of your video.", "Adding a Video to your website will boost sales and increase leads.", "We pride ourselves on our high quality and successful Spokespeople and professionally created videos.", "Our videos are all shot and built here at our headquarters to maintain quality and precision. We do not outsource our filming to any other studios. It is all filmed HERE."
@@ -14,61 +12,16 @@ $(document).ready(function () {
 		var d = new Date();
 		var year = d.getFullYear();
 		$('#year').text(year);
-
-		function getAbsolutePath() {
-			var loc = window.location;
-			var path = loc.pathname.replace('/', '');
-			var pathName = path.replace('/', '');
-			if (!pathName.trim()) {
-				pathName = 'home';
-			}
-			console.log(pathName);
-			return pathName;
-		}
-		switch (getAbsolutePath()) {
-			case 'specials':
-				$('#specials').addClass('active');
-				break;
-			case '360_degree_video':
-				$('#threesixty').addClass('active');
-				break;
-			case 'spokespeople':
-				$('#spokespeople').addClass('active');
-				break;
-			case 'Custom_Video_Presentation':
-				$('#presentations').addClass('active');
-				break;
-			case 'Website_Spokesperson':
-				$('#web').addClass('active');
-				break;
-			case 'specials':
-				$('#specials').addClass('active');
-				break;
-			case 'home':
-				$('#home').addClass('active');
-				break;
-		}
-		var windowWidth = $(window).width();
-		if (windowWidth > 767) {
-			var headerWidth = $('.nav-container').outerWidth();
-			var leftWidth = $('.navbar-brand').outerWidth();
-			var centerWidth = $('.navbar-mine').outerWidth();
-			var rightWidth = $('.navbar-right').outerWidth();
-			if (windowWidth > 991) {
-				var pad = (headerWidth - leftWidth - centerWidth - rightWidth) / 2;
-				$('.navbar-mine').css('padding-left', pad);
-			} else {
-				$('.navbar-brand').css('padding-left', (windowWidth - leftWidth) / 2 + 'px');
-				$('.navbar-mine').css('padding-left', (windowWidth - centerWidth) / 2 + 'px');
-				$('.navbar-right').css('padding-right', (windowWidth - rightWidth) / 2 + 'px');
-				console.log(windowWidth);
-			}
-		}
-
 	});
 });
 (function ($) {
 	'use strict';
+	$.fn.padAward = function () {
+		console.log( $('#best-video').height() );
+		console.log( $('#best-video').width() );
+		var p = ($('#best-video').width() - $('#best-video').height())/2;
+		$('#best-video').css("padding", p + "px 0");
+	};
 	$.fn.padHeight = function () {
 		var base = this[0];
 		var holder = this.closest('.holder');
@@ -89,16 +42,15 @@ $(document).ready(function () {
 	};
 }(jQuery));
 //nav
-$(document).ready(function(){
+$(document).ready(function () {
 	var title = $(document).find("title").text();
-	console.log( title );
-	if(title.indexOf("About")>-1){
+	if (title.indexOf("About") > -1) {
 		$('#about').addClass("active");
-	}else if(title.indexOf("Styles")>-1){
+	} else if (title.indexOf("Styles") > -1) {
 		$('#styles').addClass("active");
-	}else if(title.indexOf("Spokespeople")>-1){
+	} else if (title.indexOf("Spokespeople") > -1) {
 		$('#spokespeople').addClass("active");
-	}else{
+	} else {
 		$('#home').addClass("active");
 	}
 })
---
+++ virtual-spokespeople/index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+	<title>Virtual Spokespeople</title>
+	<meta content="width=device-width, initial-scale=1" name="viewport">
+	<meta content="Professional Spokesperson, Video Spokesperson, Virtual Spokesperson, Website Spokesperson, Web Spokesperson, Spokes-model, Walk On Actor, Virtual Actor, Person On Website, Online Spokesperson" name="keywords">
+	<meta content="Learn about Virtual Spokespeople from Live-Spokesperson.com. Our professionally created spokesperson video will help super-charge your website" name="description">
+	<meta content="index, follow" name="robots">
+	<!-- (Robot commands: All, None, Index, No Index, Follow, No Follow) -->
+	<meta content="30 days" name="revisit-after">
+	<meta content="global" name="distribution">
+	<meta content="general" name="rating">
+	<meta content="english" name="Content-Language">
+	<meta content="YNESpqoAwK51PmBV7/PFKLG0agx7AQPKhXXcYAXGGF8=" name="verify-v1">
+	<meta content="iinbv24r-1ix20hgj5l94wz2rnn3aiwi0336hwysvvpiskquy6ijsh9wy12f3znbed-hz1ay8ppzhgqap-sicqtw6ui29d0wrfcpenudh1ml9xwjbej7u25xy9pnm6yr" name="norton-safeweb-site-verification">
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-j8y0ITrvFafF4EkV1mPW0BKm6dp3c+J9Fky22Man50Ofxo2wNe5pT1oZejDH9/Dt" crossorigin="anonymous">
+	<link href="https://vjs.zencdn.net/7.2.4/video-js.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-LRlmVvLKVApDVGuspQFnRQJjkv0P7/YFrw84YYQtmYG4nK8c+M+NlmYDCv0rKWpG" crossorigin="anonymous">
+	<link rel="stylesheet" href="http://www.live-spokesperson.com/css/bootstrap.css">
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</head>
+
+<body>
+	<?php include("../includes/header.php"); ?>
+	<section class="jumbotron">
+		<div>
+			<h1 class="text-center">Virtual Spokespeople<i class="far fa-registered smaller"></i></h1>
+			<div class="row align-items-center">
+				<div class="col-xl-6 offset-1" id="video-holder">
+					<?php     $video = "Professional Spokespeople";$bgColor = "#757575";$alt = "Some of Our Professional Spokespeople";include("../includes/showVideo.php");?>
+				</div>
+				<div class="col-xl-4">
+					<?php include( "../includes/contact-awards.php"); ?>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="alert alert-info">
+		<h2 class="text-center">Virtual Spokespeople Info</h2>
+		<div class="">
+			<div class="card-group">
+				<div class="card mb-3">
+					<div class="card-header line-1 text-capitalize">Our Virtual Spokesperson Will:<i class="float-right fal fa-user-chart"></i>
+					</div>
+					<div class="card-body">
+						<ul class="list-group list-group-flush">
+							<li class="list-group-item">Give Your Website Credibility</li>
+							<li class="list-group-item">Make Viewers Empathize With You</li>
+							<li class="list-group-item">Get Them to Trust You</li>
+							<li class="list-group-item">Increase Leads And Conversions</li>
+						</ul>
+					</div>
+				</div>
+				<div class="card mb-3">
+					<div class="card-header line-1">Get Visitors to:<i class="float-right far fa-user"></i></i>
+					</div>
+					<div class="card-body">
+						<ul class="list-group list-group-flush">
+							<li class="list-group-item">Order</li>
+							<li class="list-group-item">Enter Their Contact Information</li>
+							<li class="list-group-item">Subscribe to Your Newsletter</li>
+							<li class="list-group-item">Go to a Specific Page</li>
+						</ul>
+					</div>
+				</div>
+				<div class="card mb-3">
+					<div class="card-header line-1 text-capitalize">Trusted by 1000+ businesses for:<i class="float-right far fa-thumbs-up"></i>
+					</div>
+					<div class="card-body">
+						<ul class="list-group list-group-flush">
+							<li class="list-group-item">Professional Spokespeople</li>
+							<li class="list-group-item">Informational Whiteboard</li>
+							<li class="list-group-item">Animation Videos</li>
+							<li class="list-group-item">Excellent Technical Support</li>
+							<li class="list-group-item">Spokesperson Video</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</section>
+	<section class="container-fluid mb-3">
+		<div class="card-group">
+			<div class="card card-primary">
+				<div class="card-header header-primary">
+					<h2 class="text-center">Website Spokesperson</h2>
+				</div>
+				<div class="card-img-top">
+					<?php $btnColor="#3D9CEB";$bgColor = "#2C68B3"; $video="Video Background"; $alt="Video Background"; include("../includes/showCardVideo.php"); ?>
+				</div>
+				<div class="card-body">
+					<div class="content">
+						<p>Web developers know us best for Developing top-notch, high resolution, and spokesperson video. Customized website spokespersons video suited for various needs. Easy installation and integration. </p>
+						<p> Companies who want to stay competitive and relevant are moving to Web Video. We understand web video and will work with you to make the best video for you.</p>
+						<p>It is one of the best investments to your website and you can expect the return to be massive. Get started today- get in touch with us now to discuss your requirements.</p>
+					</div>
+				</div>
+			</div>
+			<div class="card card-primary">
+				<div class="card-header header-primary">
+					<h2 class="text-center">Spokesperson Software</h2>
+				</div>
+				<div class="card-img-top">
+					<?php $btnColor="#3D9CEB";$bgColor = "#7298B2"; $video="Professional Spokesperson"; $alt="Angie-Professional Spokesperson"; include("../includes/showCardVideo.php"); ?>
+				</div>
+				<div class="card-body">
+					<div class="content">
+						<p>Video spokespeople are different from the auto-play videos that are in a box that you see in most websites. A virtual spokesperson is a border-less video with a transparent background.   It looks like the spokesperson stepped out of the website. This will grab the attention of website visitors, so they will listen to the spokesperson.</p>
+						<p>Website spokesperson software should allow you to host your own video on your own server. There will be nothing you could do if they 'accidentally' remove your video from their servers. A website spokesperson video should only have a one-time fee, not a monthly fee.</p>
+					</div>
+				</div>
+			</div>
+			<div class="card card-primary">
+				<div class="card-header header-primary">
+					<h2 class="text-center">Website Toolbox</h2>
+				</div>
+				<div class="card-img-top">
+					<?php $btnColor="#3D9CEB";$bgColor = "#BCE4FF"; $video="Virtual Spokespeople"; $alt="Many of our Professional Virtual Spokespeople"; include("../includes/showCardVideo.php"); ?>
+				</div>
+				<div class="card-body">
+					<div class="content">
+						<p>If you want to get the most of your website and marketing effort, you need a website spokesperson. Hands down one of the sharpest tools you can add to your websites' toolbox. If you're serious about increasing conversion and boosting leads get one today.</p>
+						<p>But, any website spokesperson just doesn't cut it. So, you need to get one that is professional and tailor-made for your needs. A video dedicated to your goals, brand, and unique selling proposition is what you need. They'll get the goods delivered with less hassle.</p>
+					</div>
+				</div>
+			</div>
+	</section>
+	<?php include("../includes/awards.php"); ?>
+	<?php include("../includes/footer.php"); ?>
+	<?php include("../includes/modal.php");?>
+	<script src="https://vjs.zencdn.net/7.2.4/video.js"></script>
+	<script src="../js/iVideo.js"></script>
+	<script src="../js/nav.js"></script>
+
+</body>
+</html>
