0e61b28de825eda8f5cfc784d659b47ceee8e512
Andy Knudsen <andy@websitetalkingheads.com>
2019-03-06 18:54:33 -0700

    updated paths

--- includes/head.php
+++ includes/head.php
@@ -1,13 +1,11 @@
 <link rel="icon" href="/favicon.ico">
 <!--CSS-->
 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-6jHF7Z3XI3fF4XZixAuSu0gGKrXwoX/w3uFPxC56OtjChio7wtTGJWRW53Nhx6Ev" crossorigin="anonymous">
-<link href="http://www.live-spokesperson.com/css/bootstrap.css" rel="stylesheet" type="text/css">
-<link href="http://www.live-spokesperson.com/css/video-js.css" rel="stylesheet">
-<link rel="stylesheet" href="https://use.typekit.net/tda0xaa.css">
+<link href="/css/bootstrap.css" rel="stylesheet" type="text/css">
+<link href="/css/video-js.css" rel="stylesheet">
+<link rel="stylesheet" href="https://use.typekit.net/xtn2ztk.css">
 <!--Java Script-->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 <script src="https://vjs.zencdn.net/7.2.4/video.js"></script>
-<script src="http://www.live-spokesperson.com/js/iVideo.js"></script>
-<script src="http://www.live-spokesperson.com/js/custom.js"></script>
