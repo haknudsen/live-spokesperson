0ad22b5b94828530ead1ce82f9ef6899aeb14ea8
Andy Knudsen <paris_giovanni@yahoo.com>
2019-03-25 17:31:38 -0600

    working on estate planning

--- estate-planning/index.php
+++ estate-planning/index.php
@@ -1,78 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Estate Planning-Live Spokesperson</title>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<meta content="Searching for a new way to bring life to your website and attract more interest from those who visit it? Get an professional spokesperson from Live-Spokesperson.">
-	<?php include("../includes/head.php"); ?>
+    <meta charset="UTF-8">
+    <title>Estate Planning-Live Spokesperson</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta content="Searching for a new way to get more clients?  A video from Talking Heads® can engage, educate and inform viewers.">
+    <?php include("../includes/head.php"); ?>
 </head>
 
 <body>
-	<?php include("../includes/nav.php"); ?>
-	<section class="jumbotron">
-		<div class="row align-items-center">
-			<div class="col-xl-6 offset-1" id="video-holder">
-				<div class="embed-responsive embed-responsive-16by9">
-					<iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="https://www.youtube.com/embed/2EhlppFns_w?autoplay=0&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0&origin=https://youtubeembedcode.com"></iframe>
-				</div>
-			</div>
-			<div class="col-xl-4">
-				<?php include( "../includes/contact.php"); ?>
-			</div>
-		</div>
-	</section>
-	<section class="container-fluid">
-		<div class="card-columns">
-			<div class="card text-white bg-primary">
-				<h2 class="card-header bg-gradient-primary">What to increase traffic up to 400%?</h3>
+    <?php include("../includes/nav.php"); ?>
+    <section class="jumbotron">
+        <div class="row align-items-center">
+            <div class="col-xl-6 offset-1" id="video-holder">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="https://www.youtube.com/embed/2EhlppFns_w?autoplay=0&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0&origin=https://youtubeembedcode.com"></iframe>
+                </div>
+            </div>
+            <div class="col-xl-4">
+                <?php include( "../includes/contact.php"); ?>
+            </div>
+        </div>
+    </section>
+    <section class="container-fluid">
+        <div class="card-columns">
+            <div class="card text-white bg-primary">
+                <h2 class="card-header bg-gradient-primary">Benefits of VIDEO!</h3>
 				<div class="card-body">
-					<h5 class="card-title">ADD VIDEO!</h5>
-					<p class="card-text">Are you a website owner? Are you struggling to GET MORE SALES even though you have tried everything? Maybe you’ve tried SEO, SEP, PPC, and email. But your sales are not what you think they should be yet. In fact, they aren’t even close to what you REALLY want. Are you getting frustrated? Are you looking for ways to REALLY boost sales? Or maybe you haven’t tried anything yet because it all seems so confusing…. What if you could get both more traffic and more sales. We can help.</p>
-				</div>
-			</div>
-			<div class="card text-white bg-primary">
-				<h3 class="card-header bg-gradient-primary text-capitalize">Add video to your site</h3>
-				<div class="card-body">
-					<h5 class="card-title">BOOST Traffic and Rocket Sales!</h5>
-					<p class="card-text">You heard me. An internet guru just reported that video is <strong>50X's</strong> more likely to appear in the top ten than your website. Let me repeat that: Internet video is 50X’s more likely to come up in the TOP TEN on search engines.</p>
-				</div>
-			</div>
-			<div class="card text-white bg-primary">
-				<h3 class="card-header bg-gradient-primary text-capitalize">What's Your Video Strategy?</h3>
-				<div class="card-body">
-					<h5 class="card-title">You <strong>Need</strong> a Video Strategy</h5>
-					<p class="card-text">If you don’t have a video strategy, <span class="red">FILL OUT THE FORM</span> right now. Or call. The phone number is on the top of the screen. We will talk with you about these amazing opportunities to make your site the best. Fill out the form on this page and our <strong>video expert</strong> will contact you and give you a <strong>FREE CONSULTATION</strong>.</p>
+					<h5 class="card-title text-capitalize">How can I get the attention of proscpective clients?</h5>
+					<p class="card-text"> As a estate planner or lawyer, it’s vital that you leverage every means available to capture the attention of your prospective clients. One of the best ways to do this is with online video.  Online Video will;</p>
+                    <ul class="list-group list-group-flush">
+                       <li class="list-group-item bg-primary">Catch The Attention Of Your Viewers</li>
+                       <li class="list-group-item bg-primary">Explain Your Process</li>
+                       <li class="list-group-item bg-primary">Engage Your Viewers</li>
+                       <li class="list-group-item bg-primary">Communicate Your Message</li>
+                       <li class="list-group-item bg-primary">Develop Rapport</li>
+                    </ul>
 				</div>
 			</div>
 			<div class="card text-white bg-primary">
-				<h3 class="card-header bg-gradient-primary text-capitalize">Do you need more info?</h3>
+				<h3 class="card-header bg-gradient-primary text-capitalize">Video for Estate Planning</h3>
 				<div class="card-body">
-					<h5 class="card-title">OK! Here’s more:</h5>
-					<p class="card-text">78% feel that online video ads provide as much or more of an opportunity to learn about a product or service.</p>
-					<p class="card-text">People want you to have videos. The want to learn more. Psychological studies say that we humans learn better and stay more alert when VIDEO, AUDIO, and TEXT are all used together. You will engage your visitors better. Have you watch the video here yet. If not, do it now. It gives you the same info, but in a way that TEXT ALONE CAN’T DELIVER</p>
+					<h5 class="card-title">Educate Prospective Clients</h5>
+					<p class="card-text">Estate planning is necessary to determine short and long term goals and develop strategies to meet these goals. Some understand these concepts and estate planning can benefit them. Yet, many don’t. So you need to educate them about the benefits and process of Estate Planning.</p>
 				</div>
 			</div>
 			<div class="card text-white bg-primary">
-				<h3 class="card-header bg-gradient-primary text-capitalize">Get people to take action</h3>
+				<h3 class="card-header bg-gradient-primary text-capitalize">Content They Can't Resist</h3>
 				<div class="card-body">
-					<h5 class="card-title">64% say they have taken ACTION after seeing an online video.</h5>
-					<p class="card-text">What? 64% have taken action? Are you kidding me? What else do I need to tell you? People are taking action. This is unbelievable. Do I really have to keep typing? Yes, I do. Because you don’t change easily. You need a personal invitation.</p>
+					<h5 class="card-title">Engage Prospective Clients</h5>
+					<p class="card-text">A video from an experienced video production company will engage your prospective clients with content they can't resist. Your estate planning video will have the empathy you need to gain the confidence of your audience, build brand trust and generate leads. With online video, you get the chance to show your audience what you do in a way they can relate to and understand.</p>
 				</div>
 			</div>
 			<div class="card text-white bg-primary">
-				<h3 class="card-header bg-gradient-primary text-capitalize">For best video results</h3>
+				<h3 class="card-header bg-gradient-primary text-capitalize">Experienced</h3>
 				<div class="card-body">
-					<h5 class="card-title text-uppercase">How-TO Videos</h5>
-					<p class="card-text">Your brand can be a hundred different things, but there is always room for making how-to videos. </p>
-					<ul class="list-group list-group-indent">
-						<li>Behind The Scenes Video</li>
-						<li>Product Demonstrations</li>
-						<li>Website Walkthrough</li>
-						<li>How to Use Your Product</li>
-					</ul>
+					<h5 class="card-title">Video Creators</h5>
+					<p class="card-text">With over 10 years of experience in web video production, you’re guaranteed to get the best from Talking Heads®. Unlike other video production companies, we don’t ask you to fill out a lame generic survey, and we don’t use freelance writers. We take the time to listen to your unique goals and needs and discuss them with you. When we’re on the same page, we will collaborate with you live to create a power custom script that’ll engage and motivate your viewers to take action.</p>
 				</div>
 			</div>
 			<div class="card text-white bg-primary">
@@ -87,7 +73,7 @@
 				<h3 class="card-header bg-gradient-primary text-capitalize">IF YOU HAVE A WEBSITE YOU NEED VIDEO</h3>
 				<div class="card-body">
 					<h5 class="card-title">Improve Search Results</h5>
-					<p class="card-text">You might not be a media company, but you should still consider the perks of having videos for your products and services. Google loves video because they own the largest video platform in the world, YouTube.</p>
+					<p class="card-text">Besides building trust and boosting conversion rates for your firm, a well-structured video will increase your chances of ranking on Google and other search engines. Because people share emotions, not facts, they’re more likely to share your videos, which will increase your chances of exposure to your prospective clients.</p>
 					<p class="card-text">A well optimized video is more than <span class="text-dark text-bold text-large">50x</span> more likely to appear on the first page of search results.</p>
 				</div>
 			</div>
@@ -95,8 +81,15 @@
 	</section>
 <section class="alert alert-info mt-3">
 		<div class="container">
-			<h3 class="text-center">What a Website Spokesperson Can Do For You</h3>
-			<blockquote class="text-center">Your Virtual Spokesperson could greet your online visitors to help them feel more comfortable on your website. Your website Spokesperson will build confidence that your business is actually solid giving your business a professional face and clarify parts of one's website in addition to help the visitor to know the way your products and services will give them and edge.</blockquote>
+			<h3 class="text-center">What's Your Video Strategy?</h3>
+            <div class="row">
+            <div class="col-lg-6">
+					<?php $video="Massive Wall Of Spokespeople";$alt="Massive Wall Of Spokespeople";$bgColor = "#84D6FF"; include("../includes/showInline.php"); ?>
+                </div>
+            <div class="col-lg-6">
+                <blockquote class="text-center">If you don’t have a video strategy, FILL OUT THE FORM at the top of the pageright now. Or call 801-748-2281 We will talk with you about these amazing opportunities to make your site the best. Fill out the form on this page and our video expert will contact you and give you a FREE CONSULTATION.</blockquote>
+            </div>
+            </div>
 		</div>
 	</section>
 	
