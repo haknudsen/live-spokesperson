1c678083d9ec89711914b7627e19c71a64d38f6d
Andy Knudsen <andy@websitetalkingheads.com>
2019-04-24 03:52:43 -0600

    removed color vars

--- ivideo/talking-heads-player.js
+++ ivideo/talking-heads-player.js
@@ -1,14 +1,8 @@
 // JavaScript Document
 (function ($) {
 	"use strict";
-	$.fn.createTalkingHead = function (title, primaryColor, secondaryColor, autostart, controls) {
+	$.fn.createTalkingHead = function (title, autostart, controls) {
 		//Create Player Object
-		if (primaryColor === undefined) {
-			primaryColor = "primary";
-		}
-		if (secondaryColor === undefined) {
-			secondaryColor = "secondary";
-		}
 		if (autostart === undefined) {
 			autostart = "mouse";
 		}
@@ -16,8 +10,6 @@
 			controls = "primary";
 		}
 		var talkingHeadsVideo = {
-			primaryColor: primaryColor,
-			secondaryColor: secondaryColor,
 			autostart: controls,
 			path: "https://www.websitetalkingheads.com/ivideo/videos/"
 		};
