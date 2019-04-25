8209a1b37116992b39711119d7010fa0b7ffa3e1
Andy Knudsen <andy@websitetalkingheads.com>
2019-04-24 20:30:32 -0600

    added big btn

--- ivideo/talking-heads-player.php
+++ ivideo/talking-heads-player.php
@@ -23,14 +23,15 @@
 			<p>Your browser does not support the video tag. Please Visit <a href="https://www.websitetalkingheads.com/support/" title="Please Visit Support">https://www.websitetalkingheads.com/support/</a>
 			</p>
 		</video>
+		<div id="bigPlayBtn"></div>
 		<div id='controls'>
 			<div id='btn-restart' title='replay' accesskey="R" onclick='replayVideo();'></div>
-			<div id='btn-play'  title='play' accesskey="P" onclick='playPauseVideo();'></div>
-			<div id='btn-stop'title='stop' accesskey="X" onclick='stopVideo();'></div>
+			<div id='btn-play' title='play' accesskey="P" onclick='playPauseVideo();'></div>
+			<div id='btn-stop' title='stop' accesskey="X" onclick='stopVideo();'></div>
 			<input type='range' id='volume-bar' title="volume" min='0' max='1' step='0.1' value='1'>
 			<div id='btn-volume-mute' class='mute' title='mute' onclick='muteVolume();'></div>
 			<progress id='progress-bar' min='0' max='100' value='0'>0% played</progress>
-			<div id='btn-fullscreen-enter'  title='toggle full screen' accesskey="T" onclick='toggleFullScreen();'></div>
+			<div id='btn-fullscreen-enter' title='toggle full screen' accesskey="T" onclick='toggleFullScreen();'></div>
 		</div>
 	</div>
 	<script src="talking-heads-player.js"></script>
