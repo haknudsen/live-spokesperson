ab706adf21e3a18f223499e71c52b6d926f391d6
Andy Knudsen <andy@websitetalkingheads.com>
2019-05-20 10:42:06 -0600

    update

---
+++ includes/socials.php
@@ -0,0 +1,20 @@
+<div class="social-icons">
+	<a class="social-link animated bounceInUp" href="https://www.facebook.com/websitetalkingheads/" title="Facebook" target="new">
+		<div class="social-facebook"></div>
+	</a>
+	<a class="social-link animated bounceInUp" href="https://twitter.com/TalkingHeadsVid" title="Twitter" target="new">
+		<div class="social-twitter"></div>
+	</a>
+	<a class="social-link animated bounceInUp" href="https://www.linkedin.com/company/websitetalkingheads.com/" title="LinkedIn" target="new">
+		<div class="social-linkedin"></div>
+	</a>
+	<a class="social-link animated bounceInUp" href="https://goo.gl/Wuj6Gm" title="YouTube" target="new">
+		<div class="social-youtube"></div>
+	</a>
+	<a class="social-link animated bounceInUp" href="https://www.pinterest.com/websitetalkingheadsvideo" title="Pinterest" target="new">
+		<div class="social-pinterest"></div>
+	</a>
+	<a class="social-link animated bounceInUp" href="https://www.instagram.com/websitevideopro/" title="Instagram" target="new">
+		<div class="social-instagram"></div>
+	</a>
+</div>
--- index.php
+++ index.php
@@ -24,26 +24,6 @@
 				<div class="contact-phone animated bounceInRight">
 					<a href="tel:1-801-748-2281" title="Call Now!">1-801-748-2281</a>
 				</div>
-				<div class="social-icons">
-					<a class="social-link animated bounceInUp" href="https://www.facebook.com/websitetalkingheads/" title="Facebook" target="new">
-						<div class="social-facebook"></div>
-					</a>
-					<a class="social-link animated bounceInUp" href="https://twitter.com/TalkingHeadsVid" title="Twitter" target="new">
-						<div class="social-twitter"></div>
-					</a>
-					<a class="social-link animated bounceInUp" href="https://www.linkedin.com/company/websitetalkingheads.com/" title="LinkedIn" target="new">
-						<div class="social-linkedin"></div>
-					</a>
-					<a class="social-link animated bounceInUp" href="https://goo.gl/Wuj6Gm" title="YouTube" target="new">
-						<div class="social-youtube"></div>
-					</a>
-					<a class="social-link animated bounceInUp" href="https://www.pinterest.com/websitetalkingheadsvideo" title="Pinterest" target="new">
-						<div class="social-pinterest"></div>
-					</a>
-					<a class="social-link animated bounceInUp" href="https://www.instagram.com/websitevideopro/" title="Instagram" target="new">
-						<div class="social-instagram"></div>
-					</a>
-				</div>
 			</div>
 		</div>
 	</section>
