215539bcd6d2f16ce54975db0cf0be6e4c2a5de1
Andy Knudsen <andy@websitetalkingheads.com>
2019-05-29 18:59:58 -0600

    Working on Index page
    some styleign

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 			</div>
 			<div class="col-xl-4">
 				<?php include( "includes/contact.php"); ?>
-				<div class="align-content-center contact-phone animated fadeInUp"><strong></strong>
+				<div class="align-content-center contact-phone animated fadeInUp">
 					<a href="tel:1-801-748-2281" title="Call Now!">1-801-748-2281</a>
 				</div>
 			</div>
@@ -31,47 +31,72 @@
 		<h1 class="text-center text-capitalize">what we do</h1>
 		<div class="card-deck">
 			<div class="card">
-				<div class="card-header">
+				<div class="card-header bg-gradient-mine">
 					<h3 class="card-title">Spokespeople</h3>
-				</div>				
+				</div>
 				<img alt="Spokespeople" src="images/Spokespeople.jpg" alt="Our Spokespeople" class="poster card-img-top" title="Spokespeople" data-toggle="modal" data-target=".bd-example-modal-lg" data-video="Our Spokespeople">
 				<div class="card-body">
-					<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+					<p class="card-text">We are here to serve your video spokesperson needs. We only use experienced, professional spokespeople. They have a wide-range of backgrounds and training. Including spokesmodeling, broadcast journalism, tv presenting, and acting. Our spokespeople will use their talent and experience to promote your website.</p>
+					<p class="card-text">We offer spokesperson services that include:</p>
+					<ul class="list-group list-group-flush">
+						<li class="list-group-item">Website Spokesperson</li>
+						<li class="list-group-item">Video Presentations</li>
+						<li class="list-group-item">Online Ads</li>
+						<li class="list-group-item">Video FAQs</li>
+					</ul>
 					</p>
 				</div>
-				<div class="card-footer">
-					<p class="card-text"><small class="text-muted float-right"><a href="spokespeople/">LEARN MORE</a></small>
+				<div class="card-footer bg-gradient-mine">
+					<p class="card-text"><small class="float-right"><a href="spokespeople/">LEARN MORE</a></small>
 				</div>
 			</div>
 			<div class="card">
-				<div class="card-header">
+				<div class="card-header bg-gradient-mine">
 					<h3 class="card-title">Animation</h3>
-				</div>	
+				</div>
 				<img alt="Animation Example" src="images/Animation Example.jpg" alt="Animation" class="poster card-img-top" title="Animation" data-toggle="modal" data-target=".bd-example-modal-lg" data-video="Toll Buddy">
 				<div class="card-body">
-					<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+					<p class="card-text">Animation videos stimulate your viewers' brains. They are like a time machine with each new illustration practically hypnotizing your viewer. Because of this hyper focus, the viewer also remembers more of your animation than regular videos.</p>
+					<p class="card-text">Clearly, not all animation videos are the identical, so, we work with you to make the best video for your organization. We concentrate on supercharged messages using a hand drawn illistrations that trigger the center for engagement. We make highly successful, energizing animation VIDEOS that explain your product or service.</p>
 					</p>
 				</div>
-				<div class="card-footer">
-					<p class="card-text"><small class="text-muted float-right">LEARN MORE</small>
+				<div class="card-footer bg-gradient-mine">
+					<p class="card-text"><small class="float-right"><a href="styles/animation/" title="Animation">LEARN MORE</a></small>
 				</div>
 			</div>
 			<div class="card">
-				<div class="card-header">
+				<div class="card-header bg-gradient-mine">
 					<h3 class="card-title">Product Demonstrations</h3>
-				</div>	
+				</div>
 				<img alt="Product Demonstration Example" src="images/Product Demonstration Example.jpg" alt="Card image cap" class="poster card-img-top" title="Jeds Lemonade" data-toggle="modal" data-target=".bd-example-modal-lg" data-video="Jeds Lemonade">
 				<div class="card-body">
-					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+					<p class="card-text">Often when you create a product the most challenging part is how to show it to potential customers. Product demonstrations videos can do three general things:</p>
+					<ul class="list-group list-group-flush">
+						<li class="list-group-item">Show your merchandise</li>
+						<li class="list-group-item">Show your product working</li>
+						<li class="list-group-item">How clients can use or assemble it</li>
+					</ul>
+					<p class="card-text">Our creative specialists work on an international level at branding, design and development. We will show your product in the best possible light. When we finish your videos, people will be able to see it EVERYWHRE. Including phones, tablets and laptops.</p>
 					</p>
 				</div>
-				<div class="card-footer">
-					<p class="card-text"><small class="text-muted float-right">LEARN MORE</small>
+				<div class="card-footer bg-gradient-mine">
+					<p class="card-text"><small class="float-right"><a href="product-demonstrations/" title="Product Demonstrations">LEARN MORE</a></small>
 				</div>
 			</div>
 		</div>
 	</section>
-
+	<section class="alert alert-info">
+		<div class="container text-center">
+			<h2>Talking Heads is the Leading Website Video Production Company</h2>
+			<h3>A whole bulk of services</h3>
+			<h4>We provide a wide variety of video content</h4>
+			<p class="content">Do you need fresh, up-to-date content for your business? Are you looking for only the best of the best professional authors to get the job done? Then you've come to the right place! Whether you need blog posts, product descriptions, or technical content, Content Factory provides high-quality content that will capture your visitors’ attention and increase your search engine ranking. Choose the perfect author for your project from a pool of thousands of experienced writers, each with their own expertise.</p>
+		  <div class="btn-block">
+				<button class="btn btn-dark">Request a Quote</button>
+				<button class="btn btn-light text-capitalize">more about us</button>
+			</div>
+		</div>
+	</section>
 	<?php include("includes/footer.php"); ?>
 	<?php include("includes/modal.php"); ?>
 
