fe043ee54e2115eabffcea0bc88ae324c4abfc1f
Andy Knudsen <paris_giovanni@yahoo.com>
2019-06-13 16:14:36 -0600

    creating project-demonstration page

---
+++ product-demonstrations/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Live Spokesperson Home</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta content="Searching for a new way to bring life to your website and attract more interest from those who visit it? Get an professional spokesperson from Live-Spokesperson.">
+    <?php include("../includes/head.php"); ?>
+</head>
+
+<body>
+    <?php include("../includes/nav.php"); ?>
+    <section class="jumbotron">
+        <div class="row text-center">
+            <div class="col-md-3">
+                <h3 class="btn btn-product"><a href="#standard" title="Standard Demo">Standard Demo</a></h3>
+            </div>
+            <div class="col-md-3">
+                <h3 class="btn btn-product"><a href="unboxing" title="Overhead Unboxing">Overhead Unboxing</a></h3>
+            </div>
+            <div class="col-md-3">
+                <h3 class="btn btn-product"><a href="explanation" title="Spokesperson Explanation">Spokesperson<br>Explanation</a></h3>
+            </div>
+            <div class="col-md-3">
+                <h3 class="btn btn-product"><a href="live-action" title="Live Action">Live Action<br>Demonstration</a></h3>
+            </div>
+        </div>
+    </section>
+    <section class="container-fluid">
+        <div id="standard" class="demo">
+            <h2 class="demo-title">Standard Demo</h2>
+        </div>
+        <div class="my-5"></div>
+    </section>
+    <section class="container-fluid">
+        <div id="unboxing" class="demo">
+            <h2 class="demo-title">Overhead Unboxing</h2>
+        </div>
+        <div class="my-5"></div>
+    </section>
+    <section class="container-fluid">
+        <div id="explaination" class="demo">
+            <h2 class="demo-title">Spokesperson Explanation</h2>
+        </div>
+        <div class="my-5"></div>
+    </section>
+    <section class="container-fluid">
+        <div id="live-action" class="demo">
+            <h2 class="demo-title">Live Action Demonstration</h2>
+        </div>
+        <div class="my-5"></div>
+    </section>
+    <section class="alert alert-info">
+        <h2 class="text-uppercase text-center wow bounceInUp">follow us</h2>
+        <h3 class="text-center wow bounceInUp">Join us in <strong>Social Media</strong> and Stay Tuned!</h3>
+        <?php include("../includes/social-icons.php"); ?>
+    </section>
+    <?php include("../includes/footer.php"); ?>
+    <?php include("../includes/modal.php"); ?>
+</body>
+</html>
