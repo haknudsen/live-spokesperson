4f053097848a89a7830ad11d11ad3d91b794d5ef
Andy Knudsen <paris_giovanni@yahoo.com>
2019-06-13 16:45:24 -0600

    more on page product-demonstrations

--- product-demonstrations/index.php
+++ product-demonstrations/index.php
@@ -18,13 +18,13 @@
                 <h3 class="btn btn-product"><a href="#standard" title="Standard Demo">Standard Demo</a></h3>
             </div>
             <div class="col-md-3">
-                <h3 class="btn btn-product"><a href="unboxing" title="Overhead Unboxing">Overhead Unboxing</a></h3>
+                <h3 class="btn btn-product"><a href="#unboxing" title="Overhead Unboxing">Overhead Unboxing</a></h3>
             </div>
             <div class="col-md-3">
-                <h3 class="btn btn-product"><a href="explanation" title="Spokesperson Explanation">Spokesperson<br>Explanation</a></h3>
+                <h3 class="btn btn-product"><a href="#explanation" title="Spokesperson Explanation">Spokesperson<br>Explanation</a></h3>
             </div>
             <div class="col-md-3">
-                <h3 class="btn btn-product"><a href="live-action" title="Live Action">Live Action<br>Demonstration</a></h3>
+                <h3 class="btn btn-product"><a href="#live-action" title="Live Action">Live Action<br>Demonstration</a></h3>
             </div>
         </div>
     </section>
@@ -32,25 +32,69 @@
         <div id="standard" class="demo">
             <h2 class="demo-title">Standard Demo</h2>
         </div>
-        <div class="my-5"></div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../ivideo/talking-heads-player.php?video=Social Media&autostart=mute&controls=show"></iframe>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../ivideo/talking-heads-player.php?video=Social Media&autostart=mute&controls=show"></iframe>
+                </div>
+            </div>
+        </div>
     </section>
     <section class="container-fluid">
         <div id="unboxing" class="demo">
             <h2 class="demo-title">Overhead Unboxing</h2>
         </div>
-        <div class="my-5"></div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../ivideo/talking-heads-player.php?video=Social Media&autostart=mute&controls=show"></iframe>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../ivideo/talking-heads-player.php?video=Social Media&autostart=mute&controls=show"></iframe>
+                </div>
+            </div>
+        </div>
     </section>
     <section class="container-fluid">
         <div id="explaination" class="demo">
             <h2 class="demo-title">Spokesperson Explanation</h2>
         </div>
-        <div class="my-5"></div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../ivideo/talking-heads-player.php?video=Social Media&autostart=mute&controls=show"></iframe>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../ivideo/talking-heads-player.php?video=Social Media&autostart=mute&controls=show"></iframe>
+                </div>
+            </div>
+        </div>
     </section>
     <section class="container-fluid">
         <div id="live-action" class="demo">
             <h2 class="demo-title">Live Action Demonstration</h2>
         </div>
-        <div class="my-5"></div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../ivideo/talking-heads-player.php?video=Social Media&autostart=mute&controls=show"></iframe>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../ivideo/talking-heads-player.php?video=Social Media&autostart=mute&controls=show"></iframe>
+                </div>
+            </div>
+        </div>
     </section>
     <section class="alert alert-info">
         <h2 class="text-uppercase text-center wow bounceInUp">follow us</h2>
