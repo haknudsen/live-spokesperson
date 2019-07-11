b412fdb80a4e83f6ddb94b56e640ac12579cc184
Andy Knudsen <paris_giovanni@yahoo.com>
2019-07-11 14:36:33 -0600

    working on product demo page
    some card styling

--- styles/product-demonstrations/index.php
+++ styles/product-demonstrations/index.php
@@ -1,169 +1,186 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<meta content="Website Talking Heads" name="author">
-	<meta content="Demo videos reveal how your product works, what it can do, and how consumers assemble it. With a new product it is important to show it to potential customers" name="Description">
-	<?php include("../../includes/head.php"); ?>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta content="Website Talking Heads" name="author">
+<meta content="Demo videos reveal how your product works, what it can do, and how consumers assemble it. With a new product it is important to show it to potential customers" name="Description">
+<?php include("../../includes/head.php"); ?>
 </head>
 
 <body>
-	<?php include("../../includes/nav.php"); ?>
-	<section class="container-fluid">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<div class="center top-title">
-						<h1 class="text-center">Demo Videos</h1>
-						<p>Demo Videos or Product demonstrations are videos that show your merchandise. These videos can reveal how your product works, what it can do, and how consumers assemble it. You can personalize your product demonstrations to highlight your finer points. Videos and product demonstrations increase brand awareness. But not every type of video will work. You need a professional to produce your Demo Video.</p>
-					</div>
-				</div>
-			</div>
-	</section>
-	<section class="alert alert-info mb-3">
-		<div class="container">
-			<div class="card-deck">
-				<div class="card pop">
-					<img class="poster card-img-top" data-video="Jeds Lemonade" alt="Product Demo Videos" src="../../images/demo-experiences.jpg">
-					<div class="card-body">
-						<h6><a href="#">We Create Experiences</a></h6>
-						<p class="card-text">Our <strong>product demo videos</strong> become experiences. We will show your product in the best possible light.</p>
-					</div>
-					<div class="card-footer">
-						<div class="more-link"><a href="#">READ MORE</a> </div>
-					</div>
-				</div>
-				<div class="card pop">
-					<img class="poster card-img-top" data-video="Overhead-Product-Demo-Video" alt="Overhead Product Unboxing" src="../../images/Overhead Product Demo Video.jpg">
-					<div class="card-body">
-						<h6><a href="#">Simple, Fast And Fun</a></h6>
-						<p class="card-text">When you produce a brand-new product the most challenging part can be how to reveal show it to potential customers</p>
-					</div>
-					<div class="card-footer">
-						<div class="more-link"><a href="#">READ MORE</a> </div>
-					</div>
-				</div>
-				<div class="card pop">
-					<img class="poster card-img-top" data-video="Minithrowballs Video" alt="Product Demo with Video Spokesperson" src="../../images/Minithrowballs Video.jpg">
-					<div class="card-body">
-						<h6><a href="#">Device Friendly</a></h6>
-						<p class="card-text">People will be able to see your <strong>demo videos</strong> EVERYWHRE. Including phones and Tablets.</p>
-					</div>
-					<div class="card-footer">
-						<div class="more-link"><a href="#">READ MORE</a> </div>
-					</div>
-				</div>
-			</div>
-
-		</div>
-		</div>
-	</section>
-	<section class="container-fluid">
-			<div class="row align-items-center">
-				<div class=col-md-6>
-					<div class="float-right">
-						<img class="pop poster" data-video="Sinucleanse" alt="demo video examples" src="../../images/using-demo.jpg"></a>
-					</div>
-				</div>
-				<div class=col-md-6>
-					<div class="box-mission">
-						<h3 class="text-center">Our Mission</h3>
-						<div class="text-content">
-							<h4 class="text-center">Talking Heads' mission is to Create Engaging Videos To Help People Attain Thier Goals.</h4>
-							<p class="color-black font-weight-bold">Talking Heads® has been making Web Videos for over a decade. We understand Web Video and Online marketing is our specialty. Our creative specialists work on an international level at branding, design, and development. Our <em>product demo videos</em> will show your product in the best possible light. Sometimes when you produce a product the most challenging part is how to reveal show it to potential customers. We can help you with that. When we finish your videos, people will be able to see it EVERYWHERE! Including phones and Tablets.</p>
-							<h3 class="read-more"><a href="../../product-demonstrations/about">LEARN MORE</a></h3>
-						</div>
-					</div>
-				</div>
-			</div>
-	</section>
-	<section class="alert alert-info">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-4">
-					<div class="style1 border-bottom iconbox v3">
-						<div class=box-header>
-							<div class=icon><i class="fas fa-desktop"></i>
-							</div>
-							<div class=box-title><a href="#">You Need Demo Videos</a>
-							</div>
-						</div>
-						<div class=box-content>
-							<p>With the web and social media, business relies on video to engage their audience and drive sales.</p>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="style1 border-bottom iconbox v3">
-						<div class=box-header>
-							<div class=icon><i class="fa-eye far"></i>
-							</div>
-							<div class=box-title><a href="#">Show off Your Product</a>
-							</div>
-						</div>
-						<div class=box-content>
-							<p>Use video to show your product working, what it can do, and how clients put it together.</p>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="style1 border-bottom iconbox v3">
-						<div class=box-header>
-							<div class=icon><i class="fas fa-chalkboard-teacher"></i>
-							</div>
-							<div class=box-title><a href="#">Get Your Point Across Fast</a>
-							</div>
-						</div>
-						<div class=box-content>
-							<p>Good video reveals to consumers how your product can help them in a brief burst.</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		</div>
-	</section>
-	<section class="container-fluid">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<div class=flat-title>
-						<h3 class=font-weight-3>Are you ready to be success with us?</h3>
-						<p>Tell us about your story and your project.</p>
-						<div class="style2 form-call-back">
-							<form accept-charset=utf-8 action="#" data-mailchimp=true id=subscribe-form method=post>
-								<div id=subscribe-content>
-									<div class=field-one-third>
-										<div class=field-name><input id=subscribe-name name=name placeholder="First Name">
-										</div>
-									</div>
-									<div class=field-one-third>
-										<div class=field-email><input id=subscribe-email name=email placeholder="Email Address">
-										</div>
-									</div>
-									<div class=field-one-third>
-										<div class=field-phone><input id=subscribe-phone name=phone placeholder="Phone Number">
-										</div>
-									</div>
-									<div class=btn-submit><button class="base box-shadow" id=subscribe-button type=button>SUBSCRIBE</button>
-									</div>
-								</div>
-								<div id=subscribe-msg></div>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-	<section class="alert alert-info">
-		<h2 class="text-uppercase text-center wow bounceInUp">follow us</h2>
-		<h3 class="text-center wow bounceInUp">Join us in <strong>Social Media</strong> and Stay Tuned!</h3>
-		<?php include("../../includes/social-icons.php"); ?>
-	</section>
-	<?php include("../../includes/footer.php"); ?>
-	<?php include("../../includes/modal.php"); ?>
+<?php include("../../includes/nav.php"); ?>
+<section class="container-fluid">
+<div class="container">
+<div class="row">
+  <div class="col-md-12">
+    <div class="center top-title">
+      <h1 class="text-center">Demo Videos</h1>
+      <p>Demo Videos or Product demonstrations are videos that show your merchandise. These videos can reveal how your product works, what it can do, and how consumers assemble it. You can personalize your product demonstrations to highlight your finer points. Videos and product demonstrations increase brand awareness. But not every type of video will work. You need a professional to produce your Demo Video.</p>
+    </div>
+  </div>
+</div>
+</section>
+<section class="alert alert-info mb-3">
+  <div class="container-fluid">
+    <div class="card-deck">
+      <div class="card pop"> <img class="poster card-img-top" data-video="Jeds Lemonade" alt="Product Demo Videos" src="../../images/product demo video.jpg">
+        <div class="card-body">
+          <h6><a href="#">Make Your Product Sizzle</a></h6>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Look Like a Million Bucks Without Spending it!</li>
+            <li class="list-group-item">Choose from over 5 backgrounds and Styles</li>
+            <li class="list-group-item">Eye-Popping Camera Angles Engage Customers</li>
+            <li class="list-group-item">Callouts to Sell the Features and Benefits</li>
+            <li class="list-group-item">Call To Action to Boost Conversion Rates</li>
+            <li class="list-group-item">A Picture is Worth a Thousand Words so Video is worth Millions!!!!</li>
+          </ul>
+        </div>
+        <div class="card-footer">
+          <div class="more-link"><a href="#">READ MORE</a> </div>
+        </div>
+      </div>
+      <div class="card pop"> <img class="poster card-img-top" data-video="Overhead-Product-Demo-Video" alt="Overhead Product Unboxing" src="../../images/Overhead Product Demo Video.jpg">
+        <div class="card-body">
+          <h6><a href="#">Overhead Unboxing</a></h6>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Lower Bounce Rate</li>
+            <li class="list-group-item">Super Popular and Engaging Style</li>
+            <li class="list-group-item">Give Viewer a Birds Eye View</li>
+            <li class="list-group-item">Let’s Them Know What to Expect</li>
+            <li class="list-group-item">Boosts Retention</li>
+            <li class="list-group-item">Call to Action to Increase Sales</li>
+            <li class="list-group-item">Choose From Different Table Top Textures</li>
+            <li class="list-group-item">Male or Female Hands</li>
+            <li class="list-group-item">Sell More Products!!!!!</li>
+            <li class="list-group-item">Affordable</li>
+          </ul>
+        </div>
+        <div class="card-footer">
+          <div class="more-link"><a href="#">READ MORE</a> </div>
+        </div>
+      </div>
+      <div class="card pop"> <img class="poster card-img-top" data-video="Minithrowballs Video" alt="Product Demo with Video Spokesperson" src="../../images/Minithrowballs Video.jpg">
+        <div class="card-body">
+          <h6><a href="#">Video with Spokesperson</a></h6>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Boost Sales with a Spokesperson</li>
+            <li class="list-group-item">Explain Complicated or Hard to Understand Features</li>
+            <li class="list-group-item">Increase Conversion Rates with Demonstration</li>
+            <li class="list-group-item">Raise Visitor Engagement with In Dept Demonstration</li>
+            <li class="list-group-item">Add Music for Even More Pizazz!</li>
+            <li class="list-group-item">Sell the Heck out of your Products!!!</li>
+          </ul>
+        </div>
+        <div class="card-footer">
+          <div class="more-link"><a href="../../spokespeople/" title="Our Spokespeople">See Our Spokespeople</a> </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+</section>
+<section class="container-fluid">
+  <div class="row align-items-center">
+    <div class=col-md-6>
+      <div class="float-right"> <img class="pop poster" data-video="Sinucleanse" alt="demo video examples" src="../../images/using-demo.jpg"></a> </div>
+    </div>
+    <div class=col-md-6>
+      <div class="box-mission">
+        <h3 class="text-center">Our Mission</h3>
+        <div class="text-content">
+          <h4 class="text-center">Talking Heads' mission is to Create Engaging Videos To Help People Attain Thier Goals.</h4>
+          <p class="color-black font-weight-bold">Talking Heads® has been making Web Videos for over a decade. We understand Web Video and Online marketing is our specialty. Our creative specialists work on an international level at branding, design, and development. Our <em>product demo videos</em> will show your product in the best possible light. Sometimes when you produce a product the most challenging part is how to reveal show it to potential customers. We can help you with that. When we finish your videos, people will be able to see it EVERYWHERE! Including phones and Tablets.</p>
+          <h3 class="read-more"><a href="../../product-demonstrations/about">LEARN MORE</a></h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="alert alert-info">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <div class="style1 border-bottom iconbox v3">
+          <div class="box-header">
+            <div class="text-center icon"><i class="fas fa-desktop"></i> </div>
+            <div class="box-title"><a href="#">You Need Demo Videos</a> </div>
+          </div>
+          <div class="box-content">
+            <p>With the web and social media, business relies on video to engage their audience and drive sales.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="style1 border-bottom iconbox v3">
+          <div class="box-header">
+            <div class="text-center icon"><i class="fa-eye far"></i> </div>
+            <div class="box-title"><a href="#">Show off Your Product</a> </div>
+          </div>
+          <div class="box-content">
+            <p>Use video to show your product working, what it can do, and how clients put it together.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="style1 border-bottom iconbox v3">
+          <div class="box-header">
+            <div class="text-center icon"><i class="fas fa-chalkboard-teacher"></i> </div>
+            <div class="box-title"><a href="#">Get Your Point Across Fast</a> </div>
+          </div>
+          <div class="box-content">
+            <p>Good video reveals to consumers how your product can help them in a brief burst.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+</section>
+<section class="container-fluid">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class=flat-title>
+          <h3 class=font-weight-3>Are you ready to be success with us?</h3>
+          <p>Tell us about your story and your project.</p>
+          <div class="style2 form-call-back">
+            <form accept-charset=utf-8 action="#" data-mailchimp=true id=subscribe-form method=post>
+              <div id=subscribe-content>
+                <div class=field-one-third>
+                  <div class=field-name>
+                    <input id=subscribe-name name=name placeholder="First Name">
+                  </div>
+                </div>
+                <div class=field-one-third>
+                  <div class=field-email>
+                    <input id=subscribe-email name=email placeholder="Email Address">
+                  </div>
+                </div>
+                <div class=field-one-third>
+                  <div class=field-phone>
+                    <input id=subscribe-phone name=phone placeholder="Phone Number">
+                  </div>
+                </div>
+                <div class=btn-submit>
+                  <button class="base box-shadow" id=subscribe-button type=button>SUBSCRIBE</button>
+                </div>
+              </div>
+              <div id=subscribe-msg></div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="alert alert-info">
+  <h2 class="text-uppercase text-center wow bounceInUp">follow us</h2>
+  <h3 class="text-center wow bounceInUp">Join us in <strong>Social Media</strong> and Stay Tuned!</h3>
+  <?php include("../../includes/social-icons.php"); ?>
+</section>
+<?php include("../../includes/footer.php"); ?>
+<?php include("../../includes/modal.php"); ?>
 </body>
 </html>
