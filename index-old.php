1ba5754ed3300b5cb600e35501bfee20549ef6f8
Andy Knudsen <paris_giovanni@yahoo.com>
2019-08-29 16:31:58 -0600

    flip-cards in a row

--- index-old.php
+++ index-old.php
@@ -12,54 +12,128 @@
 
 <body>
 <?php include("includes/nav.php"); ?>
-<section class="container-fluid" style="min-height: 500px">
-  <div class="container my-3">
-    <div class="row">
-      <div class="col-sm-6 col-lg-4"> 
-        <!-- Card Flip -->
-        <div class="card-flip">
-          <div class="flip">
-            <div class="front even"> 
-              <!-- front content -->
-              <div class="card">
-                <div class="card-header bg-gradient-light">
-                  <h4 class="card-title">Video Presentations</h4>
-                </div>
-                <div class="card-body">
-                  <p class="card-text">Great for: Sales Videos, Product Demonstrations, Website Walk-throughs, Training Videos</p>
-                  <p class="card-text">Includes Spokesperson, Professional Editing, Motion Graphics, Lots of Images and Effects, Music...</p>
-                </div>
-                <div class="card-footer bg-gradient-mine"> <a href="styles/animation/" title="Animation">Animated Videos</a> </div>
+<section class="container-fluid my-3" style="min-height: 500px">
+  <div class="row">
+    <div class="col-sm-6 col-lg-4"> 
+      <!-- Card Flip -->
+      <div class="card-flip">
+        <div class="flip">
+          <div class="front even"> 
+            <!-- front content -->
+            <div class="card">
+              <div class="card-header bg-gradient-light">
+                <h4 class="card-title">Video Presentations</h4>
               </div>
+              <div class="card-body">
+                <p class="card-text">Great for: Sales Videos, Product Demonstrations, Website Walk-throughs, Training Videos</p>
+                <p class="card-text">Includes Spokesperson, Professional Editing, Motion Graphics, Lots of Images and Effects, Music...</p>
+              </div>
+              <div class="card-footer bg-gradient-mine"> <a class="poster" alt="Animated Video Example" data-toggle="modal" data-target=".bd-example-modal-lg" data-video="Video Presentation Show Reel">Video Presentation Example</a> </div>
             </div>
-            <div class="back even"> 
-              <!-- back content -->
-              <div class="card">
-                <div class="card-header bg-gradient-light">
-                  <h4 class="card-title">30 Second</h4>
-                  <h6 class="card-subtitle text-muted">Video Presentation</h6>
-                </div>
-                <div class="card-block"> </div>
-                <div class="card-body">
-                  <div class="pricing">
-                    <h6><span class="div text-dark">Starting at Just</span><br>
-                      <span class="price text-primary">$799</span></h6>
-                  </div>
-                </div>
-                <div class="card-footer bg-gradient-mine"> <a href="styles/animation/" title="Animation">Animated Videos</a> </div>
+          </div>
+          <div class="back even"> 
+            <!-- back content -->
+            <div class="card">
+              <div class="card-header bg-gradient-light">
+                <h4 class="card-title">30 Second</h4>
+                <h6 class="card-subtitle text-muted">Video Presentation</h6>
               </div>
+              <div class="card-body">
+                <div class="pricing">
+                  <h6><span class="starting text-dark">Starting at Just</span><br>
+                    <span class="price text-primary">$799</span></h6>
+                </div>
+              </div>
+              <div class="card-footer bg-gradient-mine"> <a class="poster" alt="Animated Video Example" data-toggle="modal" data-target=".bd-example-modal-lg" data-video="Video Presentation Show Reel">Video Presentation Example</a> </div>
             </div>
           </div>
         </div>
-        <!-- End Card Flip --> 
       </div>
+      <!-- End Card Flip --> 
+    </div>
+    <div class="col-sm-6 col-lg-4"> 
+      <!-- Card Flip -->
+      <div class="card-flip">
+        <div class="flip">
+          <div class="front even"> 
+            <!-- front content -->
+            <div class="card">
+              <div class="card-header bg-gradient-light">
+                <h4 class="card-title">Whiteboard Video</h4>
+              </div>
+              <div class="card-body">
+                <p class="card-text">We focus on supercharged messages utilizing a hand drawn whiteboard vides that trigger focus and engagement</p>
+                <p class="card-text">Includes: Collaborative Scripting, Creative Storyboarding, Custom Illustrations, Professional Voice Over, Music</p>
+              </div>
+              <div class="card-footer bg-gradient-mine"> <a class="poster" alt="Animated Video Example" data-toggle="modal" data-target=".bd-example-modal-lg" data-video="Custom vs Generic">Whiteboard Example</a> </div>
+            </div>
+          </div>
+          <div class="back even"> 
+            <!-- back content -->
+            <div class="card">
+              <div class="card-header bg-gradient-light">
+                <h4 class="card-title">30 Second</h4>
+                <h6 class="card-subtitle text-muted">Whiteboard Videos</h6>
+              </div>
+              <div class="card-body">
+                <div class="pricing">
+                  <h6><span class="starting text-dark text-uppercase">call for</span><br>
+                    <span class="price text-primary">QUOTE</span></h6>
+                </div>
+              </div>
+              <div class="card-footer bg-gradient-mine"> <a class="poster" alt="Animated Video Example" data-toggle="modal" data-target=".bd-example-modal-lg" data-video="Custom vs Generic">Whiteboard Example</a> </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- End Card Flip --> 
+    </div>
+    <div class="col-sm-6 col-lg-4"> 
+      <!-- Card Flip -->
+      <div class="card-flip">
+        <div class="flip">
+          <div class="front even"> 
+            <!-- front content -->
+            <div class="card">
+              <div class="card-header bg-gradient-light">
+                <h4 class="card-title">Animation Videos</h4>
+              </div>
+              <div class="card-body">
+                <p class="card-text">Our Animated Explainer Videos powerfully expain your product or service in a step-by-step manner.</p>
+                <p class="card-text">Includes: Collaborative Scripting, Creative Storyboarding, Custom Illustrations, Professional Voice Over, Music...</p>
+              </div>
+              <div class="card-footer bg-gradient-mine"> <a class="poster" alt="Animated Video Example" data-toggle="modal" data-target=".bd-example-modal-lg" data-video="Aliens Animated">Animated Video Example</a> </div>
+            </div>
+          </div>
+          <div class="back even"> 
+            <!-- back content -->
+            <div class="card">
+              <div class="card-header bg-gradient-light">
+                <h4 class="card-title">30 Second</h4>
+                <h6 class="card-subtitle text-muted">Animated Video</h6>
+              </div>
+              <div class="card-body">
+                <div class="pricing">
+                  <h6><span class="starting text-dark text-uppercase">call for</span><br>
+                    <span class="price text-primary">QUOTE</span></h6>
+                </div>
+              </div>
+              <div class="card-footer bg-gradient-mine"> <a class="poster" alt="Animated Video Example" data-toggle="modal" data-target=".bd-example-modal-lg" data-video="Aliens Animated">Animated Video Example</a> </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- End Card Flip --> 
     </div>
   </div>
 </section>
 <?php include("includes/footer.php"); ?>
+<?php include("includes/modal.php");?>
+    <script>
 $(function() {
 	$('.even').matchHeight(
     {byRow:false});
 });
+    </script>
 </body>
 </html>
