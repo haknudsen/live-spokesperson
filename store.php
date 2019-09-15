eda81a2b67e80fe679074234faf02f31510479c1
Andy Knudsen <andy@websitetalkingheads.com>
2019-09-15 15:36:17 -0600

    working on store page

--- store.php
+++ store.php
@@ -14,22 +14,54 @@
 <body>
 <?php include("includes/nav.php"); ?>
 <section class="jumbotron">
-  <h1 class="text-center">Free Product Demo Video</h1>
-  <div class="row align-items-center">
-    <div class="col-xl-6">
+    <h1 class="text-center">Free Product Demo Video</h1>
+    <div class="container text-center">
+        <h2 class="text-center text-capitalize">Do you have a product?</h2>
+        <h3>When you produce a brand-new product the most challenging part can be how to reveal it to the masses.</h3>
+        <h4>How is your product going to make a great first impression?</h4>
+        <h5>We can help you with a FREE Product Demonstration Video</h5>
+        <h3>With a Product Demonstratio Video You Will</h3>
+        <div class="row align-items-center">
+            <div class="col-lg-4">
+                <ul class="list-group">
+                    <li class="list-group-item">Lower Bounce Rate</li>
+                    <li class="list-group-item">Show Off Your Product</li>
+                    <li class="list-group-item">Highlight Features</li>
+                    <li class="list-group-item">Boost Retention</li>
+                    <li class="list-group-item">Engage Viewers</li>
+                </ul>
+            </div>
+            <div class="col-lg-8"> <img class="poster card-img-top" data-video="Overhead-Product-Demo-Video" alt="Overhead Product Unboxing" src="../../images/Overhead Product Demo Video.jpg"> </div>
+        </div>
     </div>
-    <div class="col-xl-6">
-      <div class="ecwid ecwid-SingleProduct-v2 ecwid-SingleProduct-v2-bordered ecwid-SingleProduct-v2-centered ecwid-Product ecwid-Product-149844386" itemscope itemtype="http://schema.org/Product" data-single-product-id="149844386"><div itemprop="image"></div><div class="ecwid-title" itemprop="name" content="Product Demo Video"></div><div itemtype="http://schema.org/Offer" itemscope itemprop="offers"><div class="ecwid-productBrowser-price ecwid-price" itemprop="price" content="0" data-spw-price-location="button"><div itemprop="priceCurrency" content="USD"></div></div></div><div customprop="options"></div><div customprop="addtobag"></div></div><script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?18447751&data_platform=singleproduct_v2" charset="utf-8"></script><script type="text/javascript">xProduct()</script>
+</section>
+<section class="container">
+    <div class="row align-items-center" style="min-height: 690px">
+        <div class="col-xl-9 text-center"> </div>
+        <div class="col-xl-3">
+            <div class="ecwid ecwid-SingleProduct-v2 ecwid-SingleProduct-v2-bordered ecwid-SingleProduct-v2-centered ecwid-Product ecwid-Product-149844386" itemscope itemtype="http://schema.org/Product" data-single-product-id="149844386">
+                <div itemprop="image"></div>
+                <div class="ecwid-title" itemprop="name" content="Product Demo Video"></div>
+                <div itemtype="http://schema.org/Offer" itemscope itemprop="offers">
+                    <div class="ecwid-productBrowser-price ecwid-price" itemprop="price" content="0" data-spw-price-location="button">
+                        <div itemprop="priceCurrency" content="USD"></div>
+                    </div>
+                </div>
+                <div customprop="options"></div>
+                <div customprop="addtobag"></div>
+            </div>
+            <script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?18447751&data_platform=singleproduct_v2" charset="utf-8"></script><script type="text/javascript">xProduct()</script> 
+        </div>
     </div>
-  </div>
+    </div>
 </section>
 <section class="container-fluid mb-2">
-  <?php include("includes/testimonials.php"); ?>
+    <?php include("includes/testimonials.php"); ?>
 </section>
 <section class="alert alert-info">
-  <h2 class="text-uppercase text-center wow bounceInUp">follow us</h2>
-  <h3 class="text-center wow bounceInUp">Join us in <strong>Social Media</strong> and Stay Tuned!</h3>
-  <?php include("includes/social-icons.php"); ?>
+    <h2 class="text-uppercase text-center wow bounceInUp">follow us</h2>
+    <h3 class="text-center wow bounceInUp">Join us in <strong>Social Media</strong> and Stay Tuned!</h3>
+    <?php include("includes/social-icons.php"); ?>
 </section>
 <?php include("includes/spokesperson-specials.php"); ?>
 <?php include("includes/footer.php"); ?>
