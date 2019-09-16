c16d5f36978e4892e65ebc28fcf36d8927fef856
Andy Knudsen <paris_giovanni@yahoo.com>
2019-09-16 17:01:22 -0600

    did some styling for product cards

--- styles/product-demonstrations/store.php
+++ styles/product-demonstrations/store.php
@@ -13,46 +13,61 @@
 
 <body>
 <section class="jumbotron">
-    <h1 class="text-center">Free Product Demo Video</h1>
-    <div class="container text-center">
-        <h2 class="text-center text-capitalize">Do you have a product?</h2>
-        <h3>When you produce a brand-new product the most challenging part can be how to reveal it to the masses.</h3>
-        <h4>How is your product going to make a great first impression?</h4>
-        <h5>We can help you with a FREE Product Demonstration Video</h5>
-        <h3>With a Product Demonstratio Video You Will</h3>
-        <div class="row align-items-center">
-            <div class="col-lg-4">
-                <ul class="list-group">
-                    <li class="list-group-item">Lower Bounce Rate</li>
-                    <li class="list-group-item">Show Off Your Product</li>
-                    <li class="list-group-item">Highlight Features</li>
-                    <li class="list-group-item">Boost Retention</li>
-                    <li class="list-group-item">Engage Viewers</li>
-                </ul>
-            </div>
-            <div class="col-lg-8"> <img class="poster card-img-top" data-video="Overhead-Product-Demo-Video" alt="Overhead Product Unboxing" src="file:///D|/images/Overhead Product Demo Video.jpg"> </div>
-        </div>
+  <h1 class="text-center">Free Product Demo Video</h1>
+  <div class="container text-center">
+    <h2 class="text-center text-capitalize">Do you have a product?</h2>
+    <h3>When you produce a brand-new product the most challenging part can be how to reveal it to the masses.</h3>
+    <h4>How is your product going to make a great first impression?</h4>
+    <h5>We can help you with a FREE Product Demonstration Video</h5>
+    <h3>With a Product Demonstratio Video You Will</h3>
+    <div class="row align-items-center">
+      <div class="col-lg-4">
+        <ul class="list-group">
+          <li class="list-group-item">Lower Bounce Rate</li>
+          <li class="list-group-item">Show Off Your Product</li>
+          <li class="list-group-item">Highlight Features</li>
+          <li class="list-group-item">Boost Retention</li>
+          <li class="list-group-item">Engage Viewers</li>
+        </ul>
+      </div>
+      <div class="col-lg-8"> </div>
     </div>
+  </div>
 </section>
 <section class="container">
-    <div class="row align-items-center" style="min-height: 690px">
-        <div class="col-xl-9 text-center"> </div>
-        <div class="col-xl-3">
-            <div class="ecwid ecwid-SingleProduct-v2 ecwid-SingleProduct-v2-bordered ecwid-SingleProduct-v2-centered ecwid-Product ecwid-Product-149844386" itemscope itemtype="http://schema.org/Product" data-single-product-id="149844386">
-                <div itemprop="image"></div>
-                <div class="ecwid-title" itemprop="name" content="Product Demo Video"></div>
-                <div itemtype="http://schema.org/Offer" itemscope itemprop="offers">
-                    <div class="ecwid-productBrowser-price ecwid-price" itemprop="price" content="0" data-spw-price-location="button">
-                        <div itemprop="priceCurrency" content="USD"></div>
-                    </div>
-                </div>
-                <div customprop="options"></div>
-                <div customprop="addtobag"></div>
+        <div class="card-deck shots">
+          <div class="card"> <img class="card-img-top" src="images/Closing Hero.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Closing Hero</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">Logo</li>
+                <li class="list-group-item">Subtitle</li>
+                <li class="list-group-item">Call To Action</li>
+                <li class="list-group-item">Website URL</li>
+              </ul>
             </div>
-            <script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?18447751&data_platform=singleproduct_v2" charset="utf-8"></script><script type="text/javascript">xProduct()</script> 
+              <div class="card-footer"><a href="#" class="btn btn-custom float-right">More</a></div>
+          </div>
         </div>
+  <div class="row align-items-center" style="min-height: 690px">
+      <div class="col-xl-9 text-center"></div>
+      <div class="col-xl-3">
+        <div class="ecwid ecwid-SingleProduct-v2 ecwid-SingleProduct-v2-bordered ecwid-SingleProduct-v2-centered ecwid-Product ecwid-Product-149844386" itemscope itemtype="http://schema.org/Product" data-single-product-id="149844386">
+          <div itemprop="image"></div>
+          <div class="ecwid-title" itemprop="name" content="Product Demo Video"></div>
+          <div itemtype="http://schema.org/Offer" itemscope itemprop="offers">
+            <div class="ecwid-productBrowser-price ecwid-price" itemprop="price" content="0" data-spw-price-location="button">
+              <div itemprop="priceCurrency" content="USD"></div>
+            </div>
+          </div>
+          <div customprop="options"></div>
+          <div customprop="addtobag"></div>
+        </div>
+        <script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?18447751&data_platform=singleproduct_v2" charset="utf-8"></script><script type="text/javascript">xProduct()</script> 
+      </div>
     </div>
-    </div>
+  </div>
 </section>
 </body>
 </html>
