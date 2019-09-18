90fcab56bc78356018bfa115db8ecbb32b0a4e5b
Andy Knudsen <andy@websitetalkingheads.com>
2019-09-18 15:13:13 -0600

    working on landing page

---
+++ styles/product-demonstrations/single-page.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Live Spokesperson Home</title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="description" content="Searching for a new way to bring life to your website and attract more interest from those who visit it? Get an professional spokesperson from Live-Spokesperson.">
+<meta name="Keywords" content="Video Solutions,Product Videos, Video Examples, Animated Video Production, Online Videos, Great Explainer Video, Importance Of Video Marketing, Video Experts, Custom Video Production Services, Web Video Production, New Animated Video, Website Videos, Web Video Services, Web Videos, Video Production Companies, Internet Video, Homepage Web Video, Web Video Production Strategy, Web Video Production, Web Video Production Company Instructional Video">
+<?php include("../../includes/head.php"); ?>
+</head>
+
+<body>
+<!-- The fixed navbar will overlay your other content, unless you add padding to the bottom of the <body>. Tip: By default, the navbar is 50px high.  -->
+<section class="container-fluid bg-img">
+    <nav class="navbar navbar-expand-lg"> <a class="navbar-brand" href="#">Demo Videos</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav m-auto">
+                <li class="nav-item active"> <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a> </li>
+                <li class="nav-item"> <a class="nav-link"  href="#demo" class="white">Demo</a> </li>
+                <li class="nav-item"> <a class="nav-link"  href="#faq" class="white">F.A.Q.</a> </li>
+                <li class="nav-item"> <a class="nav-link"  href="#bonus" class="white">Bonus</a></li>
+                <li class="nav-item"> <a class="nav-link"  href="#demo" class="white">Demo</a> </li>
+            </ul>
+            <h5 class="navbar-brand">1-801-748-2281</h5>
+        </div>
+    </nav>
+    <div class="center-align text-center">
+        <div class="strip">Get a Unique HD VIDEO, Attract More Eyeballs & Crush your COMPETITION </div>
+        <h1 class="text-warning wow infinite jello delay-2s">Turbocharge your <strong class="wow animated infinite bounce delay-2s">SALES</strong></h1>
+        <h3 class="text-light">Sometimes When You Develop A Brand-new Product the Hardest Part is <em>Brainstorming</em> the Easiest Way to Reveal It to The Masses.</h3>
+        <h4 class="text-light text-capitalize">How is your <strong class="text-warning">Product</strong> going to make that impression?</h4>
+        <h4 class="text-info">We Believe in Our Videos So Much That We Are Willing To Give Away a <strong class="free text-danger">Free</strong> Video Just to Prove Our Point</h4>
+        <div class="row mt-4">
+            <div class="col-md-2"></div>
+            <div class="col-md-8  col-sm-12">
+                <div class="center-align">
+				<div class="embed-responsive embed-responsive-16by9">
+					<iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../../ivideo/talking-heads-player.php?video=Demonstration-Advertising-Example&autostart=mute&controls=true"></iframe>
+				</div>
+                </div>
+            </div>
+        </div>
+        <h3 class="mt5 white"><span class="b9 yellow">5000+ Customers Use StockNation</span> as their Favourite Choice for <br>
+            Stock Videos selection <u>Every Single Day</u>…</h3>
+        <div class="row mt5">
+            <div class="col-md-6 col-sm-12">
+                <ul class="green-icon-list white">
+                    <li>Dominate your Competition by making <strong>better Quality Unique Videos</strong> using StockNation</li>
+                    <li><strong>25,000+ HD</strong> Videos spread <strong>across every possible Niche</strong> online</li>
+                    <li>A <strong>Searchable SAAS app</strong> (easy to find what you are looking for with simple Keywords)</li>
+                    <li><strong>Go Viral</strong> by making better videos using StockNation</li>
+                    <li><strong>Rank better on Youtube</strong> by using HD Videos in your work</li>
+                    <li>Give a complete new life to your content</li>
+                    <li><strong>Super Easy 3 click access</strong> just Search -> Download -> Use</li>
+                    <li>Brand <strong>New Drag and Drop Video Editor</strong></li>
+                </ul>
+            </div>
+            <div class="col-md-6 col-sm-12">
+                <ul class="green-icon-list white">
+                    <li>Create <strong>Studio Quality Videos</strong> in Minutes</li>
+                    <li><strong>Ultra-Fast Rendering</strong> Engine</li>
+                    <li><strong>Go Limitless</strong> (Make Unlimited Video Projects, Download Unlimited Videos, and Get Unlimited Renders)</li>
+                    <li><strong>Mix Videos, Images, Add Text over Videos and Rearrange Media</strong> to make your Unique Storyline</li>
+                    <li>No need to save 280GB Data files to your Hard Drive</li>
+                    <li>Plug & Play: <strong>Fully Hosted & Managed On The Cloud.</strong> No Download or Configuration Needed</li>
+                    <li>No Technical Experience Required</li>
+                    <li><strong>Make Unlimited Videos</strong> for Personal Projects.</li>
+                </ul>
+            </div>
+        </div>
+        <div style="display:block;text-align:center;margin:20px 0;">
+            <div class="timeout_app_clock_container_5d469f7cdfa9757e058b45bb"></div>
+        </div>
+        <h1 class="b9 yellow mt5">OUR STOCK, YOUR STORY</h1>
+        <div class="timer mt3"> 
+            <!-- <h3 class="white">Price will increase to <b style="font-size:2rem">$67</b> in...</h3> --> 
+            <!-- <div class="let_setcom_clock_container_5b9b7bd2ad367b36438b4589"></div> --> 
+        </div>
+        <div class="mt2"> <a class="button-primary" href="#buy"><span class="white b9">Get Instant Access<br>
+            <span class="h3 h3-res b7">to
+            StockNation At A One Time Price</span></span></a> </div>
+        <div class=""> <img class="seal-res" width="40%" src="https://stocknation1.imgix.net/join/img/seal-white.png"> </div>
+    </div>
+</section>
+</body>
+</html>
