685b44fcb4ddcc528ef12f793f19a48b6262584b
Andy Knudsen <andy@websitetalkingheads.com>
2019-09-18 20:10:16 -0600

    added more stuff to single-page

--- styles/product-demonstrations/single-page.php
+++ styles/product-demonstrations/single-page.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<title>Live Spokesperson Home</title>
+<title>Product Demonstration Videos</title>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -12,78 +12,172 @@
 </head>
 
 <body>
-<!-- The fixed navbar will overlay your other content, unless you add padding to the bottom of the <body>. Tip: By default, the navbar is 50px high.  -->
 <section class="container-fluid bg-img">
-    <nav class="navbar navbar-expand-lg"> <a class="navbar-brand" href="#">Demo Videos</a>
+    <nav class="navbar navbar-expand-lg"> <a class="navbar-brand text-white" href="#">Demo Videos</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav m-auto">
                 <li class="nav-item active"> <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a> </li>
-                <li class="nav-item"> <a class="nav-link"  href="#demo" class="white">Demo</a> </li>
-                <li class="nav-item"> <a class="nav-link"  href="#faq" class="white">F.A.Q.</a> </li>
-                <li class="nav-item"> <a class="nav-link"  href="#bonus" class="white">Bonus</a></li>
-                <li class="nav-item"> <a class="nav-link"  href="#demo" class="white">Demo</a> </li>
+                <li class="nav-item"> <a class="nav-link text-white"  href="#demo" class="white">Demo</a> </li>
+                <li class="nav-item"> <a class="nav-link text-white"  href="#faq" class="white">F.A.Q.</a> </li>
+                <li class="nav-item"> <a class="nav-link text-white"  href="#bonus" class="white">Bonus</a></li>
+                <li class="nav-item"> <a class="nav-link text-white"  href="#demo" class="white">Demo</a> </li>
             </ul>
-            <h5 class="navbar-brand">1-801-748-2281</h5>
+            <h5 class="navbar-brand text-white">1-801-748-2281</h5>
         </div>
     </nav>
     <div class="center-align text-center">
-        <div class="strip">Get a Unique HD VIDEO, Attract More Eyeballs & Crush your COMPETITION </div>
+        <h3 class="strip">Get a Unique HD VIDEO, Attract More Eyeballs & Crush your COMPETITION</h3>
         <h1 class="text-warning wow infinite jello delay-2s">Turbocharge your <strong class="wow animated infinite bounce delay-2s">SALES</strong></h1>
         <h3 class="text-light">Sometimes When You Develop A Brand-new Product the Hardest Part is <em>Brainstorming</em> the Easiest Way to Reveal It to The Masses.</h3>
         <h4 class="text-light text-capitalize">How is your <strong class="text-warning">Product</strong> going to make that impression?</h4>
         <h4 class="text-info">We Believe in Our Videos So Much That We Are Willing To Give Away a <strong class="free text-danger">Free</strong> Video Just to Prove Our Point</h4>
-        <div class="row mt-4">
+        <div class="row my-4">
             <div class="col-md-2"></div>
             <div class="col-md-8  col-sm-12">
                 <div class="center-align">
-				<div class="embed-responsive embed-responsive-16by9">
-					<iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../../ivideo/talking-heads-player.php?video=Demonstration-Advertising-Example&autostart=mute&controls=true"></iframe>
-				</div>
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../../ivideo/talking-heads-player.php?video=Demonstration-Advertising-Example&autostart=mute&controls=true"></iframe>
+                    </div>
                 </div>
             </div>
         </div>
-        <h3 class="mt5 white"><span class="b9 yellow">5000+ Customers Use StockNation</span> as their Favourite Choice for <br>
-            Stock Videos selection <u>Every Single Day</u>…</h3>
-        <div class="row mt5">
-            <div class="col-md-6 col-sm-12">
-                <ul class="green-icon-list white">
-                    <li>Dominate your Competition by making <strong>better Quality Unique Videos</strong> using StockNation</li>
-                    <li><strong>25,000+ HD</strong> Videos spread <strong>across every possible Niche</strong> online</li>
-                    <li>A <strong>Searchable SAAS app</strong> (easy to find what you are looking for with simple Keywords)</li>
-                    <li><strong>Go Viral</strong> by making better videos using StockNation</li>
-                    <li><strong>Rank better on Youtube</strong> by using HD Videos in your work</li>
-                    <li>Give a complete new life to your content</li>
-                    <li><strong>Super Easy 3 click access</strong> just Search -> Download -> Use</li>
-                    <li>Brand <strong>New Drag and Drop Video Editor</strong></li>
-                </ul>
-            </div>
-            <div class="col-md-6 col-sm-12">
-                <ul class="green-icon-list white">
-                    <li>Create <strong>Studio Quality Videos</strong> in Minutes</li>
-                    <li><strong>Ultra-Fast Rendering</strong> Engine</li>
-                    <li><strong>Go Limitless</strong> (Make Unlimited Video Projects, Download Unlimited Videos, and Get Unlimited Renders)</li>
-                    <li><strong>Mix Videos, Images, Add Text over Videos and Rearrange Media</strong> to make your Unique Storyline</li>
-                    <li>No need to save 280GB Data files to your Hard Drive</li>
-                    <li>Plug & Play: <strong>Fully Hosted & Managed On The Cloud.</strong> No Download or Configuration Needed</li>
-                    <li>No Technical Experience Required</li>
-                    <li><strong>Make Unlimited Videos</strong> for Personal Projects.</li>
-                </ul>
-            </div>
-        </div>
-        <div style="display:block;text-align:center;margin:20px 0;">
-            <div class="timeout_app_clock_container_5d469f7cdfa9757e058b45bb"></div>
-        </div>
-        <h1 class="b9 yellow mt5">OUR STOCK, YOUR STORY</h1>
-        <div class="timer mt3"> 
-            <!-- <h3 class="white">Price will increase to <b style="font-size:2rem">$67</b> in...</h3> --> 
-            <!-- <div class="let_setcom_clock_container_5b9b7bd2ad367b36438b4589"></div> --> 
-        </div>
-        <div class="mt2"> <a class="button-primary" href="#buy"><span class="white b9">Get Instant Access<br>
-            <span class="h3 h3-res b7">to
-            StockNation At A One Time Price</span></span></a> </div>
-        <div class=""> <img class="seal-res" width="40%" src="https://stocknation1.imgix.net/join/img/seal-white.png"> </div>
+    </div>
+    <div class="container py-4">
+        <h3 class="text-white">Since 2001 We Have Been Passionately Helping Businesses <strong>Improve</strong> Their Websites With Video.</h3>
     </div>
 </section>
+<section class="container-fluid bg-black text-light py-2">
+    <h2 class="text-center text-secondary">Make Your Product Sizzle</h2>
+    <div class="row mt5">
+        <div class="col-md-6 col-sm-12">
+            <ul>
+                <li>Look Like a Million Bucks Without Spending it!</li>
+                <li>Choose from over 5 backgrounds and Styles</li>
+                <li>Eye-Popping Camera Angles Engage Customers</li>
+                <li>Callouts to Sell The Features and Benefits</li>
+                <li>Call To Action to Boost Conversion Rates</li>
+                <li>A Picture is Worth a Thousand Words so Video is Worth Millions!!!!</li>
+            </ul>
+        </div>
+        <div class="col-md-6 col-sm-12">
+            <div class="text-small text-justify">
+                <p>You only get one chance to make a first impression. Make your first impression count with a sensational, expertly produced product demonstration.</p>
+                <p>Prior to the web, business counted on print media and photos to promote their products, however with the web and social networks, business are relying on product demonstration videos to engage their audience and drive sales.</p>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="alert alert-danger my-5">
+    <div class="text-center">
+        <h2 class="text-uppercase">your product, our video expertise</h2>
+        <h3 class="text-uppercase text-dark">offer Expires</h3>
+        <div class="row">
+            <div class="col-md-8">
+                <div id="countdown">
+                    <h2 class="strip float-right"><span id="hour" class="timer"></span> Hours <span id="min" class="timer"></span> Minutes <span id="sec" class="timer"></span> Seconds</h2>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="mt-3"> <a class="btn btn-primary float-left" href="#buy">Order Now</a> </div>
+            </div>
+        </div>
+    </div>
+</section>
+<div class="how-section1 bg-gradient-black p-5">
+    <div class="row align-items-center">
+        <div class="col-md-6">
+            <div class="w-75 m-auto">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../../ivideo/talking-heads-player.php?video=Overhead-Product-Demo-Video&autostart=no&controls=true"></iframe>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <h4 class="text-white">Find rewarding projects</h4>
+            <h4 class="subheading text-light">GetLance is a great place to find more clients, and to run and grow your own freelance business.</h4>
+            <p class="text-muted">Freedom to work on ideal projects. On GetLance, you run your own business and choose your own clients and projects. Just complete your profile and we’ll highlight ideal jobs. Also search projects, and respond to client invitations.
+                Wide variety and high pay. Clients are now posting jobs in hundreds of skill categories, paying top price for great work.
+                More and more success. The greater the success you have on projects, the more likely you are to get hired by clients that use GetLance.</p>
+        </div>
+    </div>
+    <div class="row align-items-center">
+        <div class="col-md-6">
+            <h4 class="text-white">Get hired quickly</h4>
+            <h4 class="subheading text-light">GetLance makes it easy to connect with clients and begin doing great work.</h4>
+            <p class="text-muted">Streamlined hiring. GetLance’s sophisticated algorithms highlight projects you’re a great fit for.
+                Top Rated and Rising Talent programs. Enjoy higher visibility with the added status of prestigious programs.
+                Do substantial work with top clients. GetLance pricing encourages freelancers to use GetLance for repeat relationships with their clients.</p>
+        </div>
+        <div class="col-md-6">
+            <div class="w-75 m-auto">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../../ivideo/talking-heads-player.php?video=Minithrowballs Video&autostart=no&controls=true"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row" align-items-center>
+        <div class="col-md-6">
+            <div class="w-75 m-auto">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../../ivideo/talking-heads-player.php?video=Best-Product-Videos&autostart=no&controls=true"></iframe>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <h4 class="text-white">Work efficiently, effectively.</h4>
+            <h4 class="subheading text-light">With GetLance, you have the freedom and flexibility to control when, where, and how you work. Each project includes an online workspace shared by you and your client, allowing you to:</h4>
+            <p class="text-muted">Send and receive files. Deliver digital assets in a secure environment.
+                Share feedback in real time. Use GetLance Messages to communicate via text, chat, or video.
+                Use our mobile app. Many features can be accessed on your mobile phone when on the go.</p>
+        </div>
+    </div>
+    <div class="row align-items-center">
+        <div class="col-md-6">
+            <h4 class="text-white">Get paid on time</h4>
+            <h4 class="subheading text-light">All projects include GetLance Payment Protection — helping ensure that you get paid for all work successfully completed through the freelancing website.</h4>
+            <p class="text-muted">All invoices and payments happen through GetLance. Count on a simple and streamlined process.
+                Hourly and fixed-price projects. For hourly work, submit timesheets through GetLance. For fixed-price jobs, set milestones and funds are released via GetLance escrow features.
+                Multiple payment options. Choose a payment method that works best for you, from direct deposit or PayPal to wire transfer and more.</p>
+        </div>
+        <div class="col-md-6">
+            <div class="w-75 m-auto">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../../ivideo/talking-heads-player.php?video=Lux Pro Demo&autostart=no&controls=true"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--Java Script--> 
+<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script> 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script> 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.4/js/tether.min.js"></script> 
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> 
+<script src="http://www.live-spokesperson.com/js/wow.min_.js"></script> 
+<script src="http://www.live-spokesperson.com/js/compressed.js"></script> 
+<script>
+    new WOW().init();
+</script> 
+<script>
+    $( document ).ready(function() {
+  setInterval(function time(){
+  var d = new Date();
+  var hours = 24 - d.getHours();
+  var min = 60 - d.getMinutes();
+  if((min + '').length == 1){
+    min = '0' + min;
+  }
+  var sec = 60 - d.getSeconds();
+  if((sec + '').length == 1){
+        sec = '0' + sec;
+  }
+  jQuery('#countdown #hour').html(hours);
+  jQuery('#countdown #min').html(min);
+  jQuery('#countdown #sec').html(sec);
+}, 1000); });
+    </script>
 </body>
 </html>
