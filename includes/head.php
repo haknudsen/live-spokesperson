aaf39cb6e9d27d083d813b0923a41c4d067b5452
Andy Knudsen <andy@websitetalkingheads.com>
2019-09-20 14:00:41 -0600

    started new landing page named white
    removed Font Awesome from scss and added as kit

--- includes/head.php
+++ includes/head.php
@@ -2,3 +2,4 @@
 <!--CSS-->
 <link href="http://www.live-spokesperson.com/css/bootstrap.css" rel="stylesheet" type="text/css">
 <link rel="stylesheet" href="https://use.typekit.net/tda0xaa.css">
+<script src="https://kit.fontawesome.com/0a8693018d.js" crossorigin="anonymous"></script>
---
+++ styles/product-demonstrations/white.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Product Demonstration Videos</title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="description" content="Searching for a new way to bring life to your website and attract more interest from those who visit it? Get an professional spokesperson from Live-Spokesperson.">
+<meta name="Keywords" content="Video Solutions,Product Videos, Video Examples, Animated Video Production, Online Videos, Great Explainer Video, Importance Of Video Marketing, Video Experts, Custom Video Production Services, Web Video Production, New Animated Video, Website Videos, Web Video Services, Web Videos, Video Production Companies, Internet Video, Homepage Web Video, Web Video Production Strategy, Web Video Production, Web Video Production Company Instructional Video">
+<?php include("../../includes/head.php"); ?>
+</head>
+
+<body style="background: white;overflow-x: hidden;width: 100%">
+<header>
+    <div class="overlay"></div>
+    <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+        <source src="https://www.websitetalkingheads.com/ivideo/videos/Demonstration-Advertising-Example.mp4" type="video/mp4">
+    </video>
+</header>
+<section class="container mt-3">
+    <div class="row text-center text-primary">
+        <div class="col-lg-3 col-sm-6"> <i class="fas fa-birthday-cake text-huge"></i>
+            <h3>Founded 2001</h3>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+            <div class="col-lg-3 col-sm-6"> <i class="fas fa-globe text-huge"></i>
+                <h3>1000s of Companies</h3>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+            <div class="col-lg-3 col-sm-6"> <i class="fad fa-film text-huge"></i>
+                <h3>50,000+ Videos</h3>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+            <div class="col-lg-3 col-sm-6"> <i class="fal fa-building text-huge"></i>
+                <h3>10,000 Buisnesses</h3>
+            </div>
+        </div>
+    </div>
+</section>
+<div class="container-fluid">
+    <div class="row">
+        <div class="side-bar" data-toggle="modal" data-target="#contactModal">CONTACT US</div>
+    </div>
+</div>
+<!--Java Script--> 
+<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script> 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script> 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.4/js/tether.min.js"></script> 
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> 
+<script src="http://www.live-spokesperson.com/js/wow.min_.js"></script> 
+<script src="http://www.live-spokesperson.com/js/compressed.js"></script> 
+<script>
+    new WOW().init();
+</script> 
+<script>
+    $( document ).ready(function() {
+  setInterval(function time(){
+  var d = new Date();
+  var hours = 24 - d.getHours();
+  var min = 60 - d.getMinutes();
+  if((min + '').length == 1){
+    min = '0' + min;
+  }
+  var sec = 60 - d.getSeconds();
+  if((sec + '').length == 1){
+        sec = '0' + sec;
+  }
+  jQuery('#countdown #hour').html(hours);
+  jQuery('#countdown #min').html(min);
+  jQuery('#countdown #sec').html(sec);
+}, 1000); });
+    </script>
+</body>
+</html>
