936b5402b1cb3c764896bc7e83eaee21bd6bbfba
Andy Knudsen <andy@websitetalkingheads.com>
2019-09-20 15:42:47 -0600

    added steps section
    includes custom css

--- styles/product-demonstrations/white.php
+++ styles/product-demonstrations/white.php
@@ -10,7 +10,6 @@
 <meta name="Keywords" content="Video Solutions,Product Videos, Video Examples, Animated Video Production, Online Videos, Great Explainer Video, Importance Of Video Marketing, Video Experts, Custom Video Production Services, Web Video Production, New Animated Video, Website Videos, Web Video Services, Web Videos, Video Production Companies, Internet Video, Homepage Web Video, Web Video Production Strategy, Web Video Production, Web Video Production Company Instructional Video">
 <?php include("../../includes/head.php"); ?>
 </head>
-
 <body class="landing-page">
 <header>
     <div class="overlay"></div>
@@ -43,8 +42,7 @@
     <h1 class="text-capitalize text-secondary">We are the video experts!</h1>
     <p class="text-primary">Since 2001 we have been creating videos for buisnesses around the world.</p>
     <div class="w-75 mx-auto">
-        <div id="demo" class="carousel slide" data-ride="carousel"> 
-            
+        <div id="demo" class="carousel slide" data-ride="carousel">
             <!-- Indicators -->
             <ul class="carousel-indicators">
                 <li data-target="#demo" data-slide-to="0" class="active"></li>
@@ -54,7 +52,6 @@
                 <li data-target="#demo" data-slide-to="4"></li>
                 <li data-target="#demo" data-slide-to="5"></li>
             </ul>
-            
             <!-- The slideshow -->
             <div class="carousel-inner">
                 <div class="carousel-item active"> <img src="https://www.websitetalkingheads.com/ivideo/videos/Kmoon Demo.jpg" class="img-fluid video" alt="Overhead Unboxing Example"> </div>
@@ -64,27 +61,76 @@
                 <div class="carousel-item"> <img src="https://www.websitetalkingheads.com/ivideo/videos/Reks.jpg" class="img-fluid video" alt="Product Demonstration-Reks"> </div>
                 <div class="carousel-item"> <img src="https://www.websitetalkingheads.com/ivideo/videos/Overhead-Product-Demo-Video.jpg" class="img-fluid video" alt="Demo Video Example"> </div>
             </div>
-            
             <!-- Left and right controls -->
             <div class="controls"> <a class="carousel-control-prev" href="#demo" data-slide="prev"> <i class="fad fa-chevron-circle-left"></i> </a> <a class="carousel-control-next" href="#demo" data-slide="next"> <i class="fad fa-chevron-circle-right"></i> </a> </div>
         </div>
     </div>
 </section>
+<hr>
+<section class="container">
+    <h2 class="text-secondary">Simple and <span class="font-weight-light">Easy</span> Process</h2>
+    <p class="text-primary">Easy On-line Ordering</p>
+    <div class="row">
+        <div class="col-lg-2">
+            <div class="step">
+                <div class="step-icon"> <i class="fad fa-shopping-cart"></i> </div>
+                <div class="step-number">Step 2</div>
+                <div class="step-content">Order Online</div>
+            </div>
+        </div>
+        <div class="col-lg-2">
+            <div class="step">
+                <div class="step-icon"> <i class="fad fa-envelope"></i> </div>
+                <div class="step-number">Step 2</div>
+                <div class="step-content">Email us your logo and tagline</div>
+            </div>
+        </div>
+        <div class="col-lg-2">
+            <div class="step">
+                <div class="step-icon"> <i class="fad fa-shipping-fast"></i> </div>
+                <div class="step-number">Step 3</div>
+                <div class="step-content">Send Us Your Product</div>
+            </div>
+        </div>
+        <div class="col-lg-2">
+            <div class="step">
+                <div class="step-icon"> <i class="fad fa-video"></i> </div>
+                <div class="step-number">Step 4</div>
+                <div class="step-content">We Film Your Product</div>
+            </div>
+        </div>
+        <div class="col-lg-2">
+            <div class="step">
+                <div class="step-icon"> <i class="fad fa-search"></i> </div>
+                <div class="step-number">Step 5</div>
+                <div class="step-content">Review Video</div>
+            </div>
+        </div>
+        <div class="col-lg-2">
+            <div class="step">
+                <div class="step-icon"> <i class="fad fa-cloud-download"></i> </div>
+                <div class="step-number">Step 6</div>
+                <div class="step-content">Download Your Video</div>
+            </div>
+        </div>
+    </div>
+</section>
+<hr>
 <section class="container-fluid">
     <h5>End</h5>
 </section>
 <div class="side-bar" data-toggle="modal" data-target="#contactModal">CONTACT US</div>
-<!--Java Script--> 
+<!--Java Script-->
 <script
-  src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script> 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script> 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.4/js/tether.min.js"></script> 
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> 
-<script src="http://www.live-spokesperson.com/js/wow.min_.js"></script> 
-<script src="http://www.live-spokesperson.com/js/compressed.js"></script> 
+  src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.4/js/tether.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src="http://www.live-spokesperson.com/js/wow.min_.js"></script>
+<script src="http://www.live-spokesperson.com/js/compressed.js"></script>
 <script>
     new WOW().init();
-</script> 
+</script>
 <script>
     $( document ).ready(function() {
   setInterval(function time(){
