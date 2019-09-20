b29f05e1cdea9762ac37df02894f93374eb625b0
Andy Knudsen <andy@websitetalkingheads.com>
2019-09-20 02:43:32 -0600

    updated wave svgs for countdown section
    added Last Chance Section

--- styles/product-demonstrations/single-page.php
+++ styles/product-demonstrations/single-page.php
@@ -13,139 +13,169 @@
 
 <body style="background: black;overflow-x: hidden">
 <section class="container-fluid bg-img">
-  <div class="center-align text-center">
-    <h3 class="strip">Get a Unique HD VIDEO, Attract More Eyeballs & Crush your COMPETITION</h3>
-    <h2 class="text-turbo text-warning">Turbocharge your <em>SALES</em> with a</h2>
-    <h3 class="text-turbo text-red jello delay-2s"><strong class="text-red large">FREE</strong></h3>
-    <h3 class="text-warning"><strong>Product Video!</strong></h3>
-    <div class="row my-4">
-      <div class="col-md-2"></div>
-      <div class="col-md-8  col-sm-12">
-        <div class="center-align">
-          <div class="embed-responsive embed-responsive-16by9">
-            <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../../ivideo/talking-heads-player.php?video=Demonstration-Advertising-Example&autostart=mute&controls=true"></iframe>
-          </div>
+    <div class="center-align text-center">
+        <h3 class="strip">Get a Unique HD VIDEO, Attract More Eyeballs & Crush your COMPETITION</h3>
+        <h2 class="text-turbo text-warning">Turbocharge your <em>SALES</em> with a</h2>
+        <h3 class="text-turbo text-red jello delay-2s"><strong class="text-red large">FREE</strong></h3>
+        <h3 class="text-warning"><strong>Product Video!</strong></h3>
+        <div class="row my-4">
+            <div class="col-md-2"></div>
+            <div class="col-md-8  col-sm-12">
+                <div class="center-align">
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../../ivideo/talking-heads-player.php?video=Demonstration-Advertising-Example&autostart=mute&controls=true"></iframe>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
+        <h4 class="text-info">We Believe in Our Videos So Much That We Are Willing To Give Away a <strong class="free text-danger">Free</strong> Video Just to Prove Our Point</h4>
     </div>
-    <h4 class="text-info">We Believe in Our Videos So Much That We Are Willing To Give Away a <strong class="free text-danger">Free</strong> Video Just to Prove Our Point</h4>
-  </div>
-  <div class="container py-4"> </div>
+    <div class="container py-4"> </div>
 </section>
 <section class="container-fluid bg-black text-light py-2">
-  <h2 class="text-center text-secondary">Make Your Product Sizzle</h2>
-  <div class="mx-auto w-75">
-    <ul class="list-group check">
-      <li class="list-group-item bg-dark">Look Like a Million Bucks Without Spending it!</li>
-      <li class="list-group-item bg-dark">Different backgrounds and Styles</li>
-      <li class="list-group-item bg-dark">Eye-Popping Camera Angles</li>
-      <li class="list-group-item bg-dark">Callouts to Sell The Features and Benefits</li>
-      <li class="list-group-item bg-dark">Call To Action to Boost Conversion Rates</li>
-      <li class="list-group-item bg-dark">A Picture is Worth a Thousand Words so Video is Worth Millions!!!!</li>
-    </ul>
-  </div>
+    <h2 class="text-center text-secondary">Make Your Product Sizzle</h2>
+    <div class="mx-auto w-75">
+        <ul class="list-group check">
+            <li class="list-group-item bg-dark">Look Like a Million Bucks Without Spending it!</li>
+            <li class="list-group-item bg-dark">Different backgrounds and Styles</li>
+            <li class="list-group-item bg-dark">Eye-Popping Camera Angles</li>
+            <li class="list-group-item bg-dark">Callouts to Sell The Features and Benefits</li>
+            <li class="list-group-item bg-dark">Call To Action to Boost Conversion Rates</li>
+            <li class="list-group-item bg-dark">A Picture is Worth a Thousand Words so Video is Worth Millions!!!!</li>
+        </ul>
+    </div>
 </section>
 <section class="wave-holder">
-  <div class="wave-top"></div>
-  <div class="bg-dark">
-    <div class="text-center">
-      <h2 class="text-uppercase">Free offer ends soon!</h2>
-      <h3 class="text-uppercase text-dark">offer Expires</h3>
-      <div class="row">
-        <div class="col-md-8">
-          <div id="countdown">
-            <h2 class="strip float-lg-right"><span id="hour" class="timer"></span> Hours <span id="min" class="timer"></span> Minutes <span id="sec" class="timer"></span> Seconds</h2>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="mt-3"> <a class="btn btn-primary float-lg-left" href="#buy">Order Now</a> </div>
-        </div>
-      </div>
+    <div class="wave-top">
+        <svg viewBox="0 0 500 120" preserveAspectRatio="none" style="height: 100%; width: 100%;">
+            <path d="M0.00,102.27 C116.00,0 304.00,28.00 500.00,89.00 L500.00,120.00 L0.00,120.00 Z" style="stroke: none;fill: #004582;"></path>
+        </svg>
     </div>
-  </div>
-  <div class="wave-bottom"></div>
+    <div class="bg-dark">
+        <div class="text-center">
+            <h2 class="text-uppercase">Free offer ends soon!</h2>
+            <h3 class="text-uppercase text-dark">offer Expires</h3>
+            <div class="row">
+                <div class="col-md-8">
+                    <div id="countdown">
+                        <h2 class="strip float-lg-right"><span id="hour" class="timer"></span> Hours <span id="min" class="timer"></span> Minutes <span id="sec" class="timer"></span> Seconds</h2>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="mt-3"> <a class="btn btn-primary float-lg-left wow rubberBand" href="#buy">Order Now</a> </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="wave-bottom">
+        <svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;">
+            <path d="M0.00,92.27 C146.83,192.92 204.00,8.39 500.00,100.03 L500.00,0.00 L0.00,0.00 Z" style="stroke: none;fill: #004582;"></path>
+        </svg>
+    </div>
 </section>
 <section class="how-section1 bg-gradient-black p-5">
-  <div class="row align-items-center mt-3">
-    <div class="col-md-6">
-      <div class="w-75 m-auto">
-        <div class="embed-responsive embed-responsive-16by9">
-          <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../../ivideo/talking-heads-player.php?video=Overhead-Product-Demo-Video&autostart=no&controls=true"></iframe>
+    <div class="row align-items-center mt-3">
+        <div class="col-md-6">
+            <div class="w-75 m-auto">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../../ivideo/talking-heads-player.php?video=Overhead-Product-Demo-Video&autostart=no&controls=true"></iframe>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    <div class="col-md-6">
-      <h3 class="text-white">Overhead Unboxing</h3>
-      <p class="text-muted">Show customers what they get when they open the box.</p>
-    </div>
-  </div>
-  <img class="mx-auto d-block mt-3" src="images/doodle.png" width="240" height="50" alt="divider"/>
-  <div class="row align-items-center mt-3">
-    <div class="col-md-6">
-      <h3 class="text-white">Professional Spokespeople</h3>
-      <p class="text-muted">We only used trained, experienced, and professional spokespeople for our videos.</p>
-    </div>
-    <div class="col-md-6">
-      <div class="w-75 m-auto">
-        <div class="embed-responsive embed-responsive-16by9">
-          <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../../ivideo/talking-heads-player.php?video=Minithrowballs Video&autostart=no&controls=true"></iframe>
+        <div class="col-md-6">
+            <h3 class="text-white">Overhead Unboxing</h3>
+            <p class="text-muted">Show customers what they get when they open the box.</p>
         </div>
-      </div>
     </div>
-  </div>
-  <img class="mx-auto d-block mt-3" src="images/doodle.png" width="240" height="50" alt="divider"/>
-  <div class="row align-items-center mt-3">
-    <div class="col-md-6">
-      <div class="w-75 m-auto">
-        <div class="embed-responsive embed-responsive-16by9">
-          <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../../ivideo/talking-heads-player.php?video=Best-Product-Videos&autostart=no&controls=true"></iframe>
+    <img class="mx-auto d-block mt-3" src="images/doodle.png" width="240" height="50" alt="divider"/>
+    <div class="row align-items-center mt-3">
+        <div class="col-md-6">
+            <h3 class="text-white">Professional Spokespeople</h3>
+            <p class="text-muted">We only used trained, experienced, and professional spokespeople for our videos.</p>
         </div>
-      </div>
-    </div>
-    <div class="col-md-6">
-      <h4 class="text-white text-capitalize">your product,<br>
-        our video expertise</h4>
-      <p class="text-muted">We have worked with everyone from Fortune 50 companies to Mom and Pop stores based out of their sewing room.</p>
-    </div>
-  </div>
-  <img class="mx-auto d-block mt-3" src="images/doodle.png" width="240" height="50" alt="divider"/>
-  <div class="row align-items-center mt-3">
-    <div class="col-md-6">
-      <h3 class="text-white">We are Experienced</h3>
-      <p class="text-muted">Since 2001 We Have Been Passionately Helping Businesses <strong>Improve</strong> Their Websites With Video.</p>
-    </div>
-    <div class="col-md-6">
-      <div class="w-75 m-auto">
-        <div class="embed-responsive embed-responsive-16by9">
-          <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../../ivideo/talking-heads-player.php?video=Lux Pro Demo&autostart=no&controls=true"></iframe>
+        <div class="col-md-6">
+            <div class="w-75 m-auto">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../../ivideo/talking-heads-player.php?video=Minithrowballs Video&autostart=no&controls=true"></iframe>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
+    <img class="mx-auto d-block mt-3" src="images/doodle.png" width="240" height="50" alt="divider"/>
+    <div class="row align-items-center mt-3">
+        <div class="col-md-6">
+            <div class="w-75 m-auto">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../../ivideo/talking-heads-player.php?video=Best-Product-Videos&autostart=no&controls=true"></iframe>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <h4 class="text-white text-capitalize">your product,<br>
+                our video expertise</h4>
+            <p class="text-muted">We have worked with everyone from Fortune 50 companies to Mom and Pop stores based out of their sewing room.</p>
+        </div>
+    </div>
+    <img class="mx-auto d-block mt-3" src="images/doodle.png" width="240" height="50" alt="divider"/>
+    <div class="row align-items-center mt-3">
+        <div class="col-md-6">
+            <h3 class="text-white">We are Experienced</h3>
+            <p class="text-muted">Since 2001 We Have Been Passionately Helping Businesses <strong>Improve</strong> Their Websites With Video.</p>
+        </div>
+        <div class="col-md-6">
+            <div class="w-75 m-auto">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../../ivideo/talking-heads-player.php?video=Lux Pro Demo&autostart=no&controls=true"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 <section class="wave-holder">
-  <div class="wave-top">
-      
-    <svg viewBox="0 0 500 120" preserveAspectRatio="none" style="height: 100%; width: 100%;">
-      <path d="M0.00,100.27 C186.83,2.92 304.30,28.39 500.00,119.03 L500.00,120.00 L0.00,120.00 Z" style="stroke: none;fill: #004582;"></path>
-    </svg>
-  </div>
-  <div class="wave-body">
-    <div class="container">
-      <div class="row align-items-center">
-        <div class="col-lg-10">
-          <h3 class="text-white">Sometimes When You Develop a New Product the Hardest Part is <span class="font-italic text-info font-weight-light">Brainstorming</span> the Easiest Way to Reveal It to The Masses.</h3>
-          <h4 class="text-light text-capitalize mb-0">How is your <strong class="text-warning">Product</strong> going to make that impression?</h4>
-        </div>
-        <div class="col-lg-2 text-huge text-center"> <i class="fal fa-desktop"></i> </div>
-      </div>
+    <div class="wave-top">
+        <svg viewBox="0 0 500 120" preserveAspectRatio="none" style="height: 100%; width: 100%;">
+            <path d="M0.00,82.27 C116.00,0 304.00,128.00 500.00,39.00 L500.00,120.00 L0.00,120.00 Z" style="stroke: none;fill: #004582;"></path>
+        </svg>
     </div>
-  </div>
-  <div class="wave-bottom">
-    <svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;">
-      <path d="M0.00,92.27 C216.83,192.92 304.30,8.39 500.00,109.03 L500.00,0.00 L0.00,0.00 Z" style="stroke: none;fill: #004582;"></path>
-    </svg>
-  </div>
+    <div class="wave-body">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-10">
+                    <h3 class="text-white">Sometimes When You Develop a New Product the Hardest Part is <span class="font-italic text-info font-weight-light">Brainstorming</span> the Easiest Way to Reveal It to The Masses.</h3>
+                    <h4 class="text-light text-capitalize mb-0">How is your <strong class="text-warning">Product</strong> going to make that impression?</h4>
+                </div>
+                <div class="col-lg-2 text-huge text-center"> <i class="fal fa-desktop"></i> </div>
+            </div>
+        </div>
+    </div>
+    <div class="wave-bottom">
+        <svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;">
+            <path d="M0.00,92.27 C216.83,192.92 304.30,8.39 500.00,109.03 L500.00,0.00 L0.00,0.00 Z" style="stroke: none;fill: #004582;"></path>
+        </svg>
+    </div>
+</section>
+<section class="container text-center mb-4">
+    <h2 class="text-warning text-uppercase">LAST chance</h2>
+    <div class="text-light text-large">to get your <span class="free text-red">FREE</span> Video!</div>
+    <div class="d-flex justify-content-center">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Free Demo Video</h5>
+                <div class="ecwid ecwid-SingleProduct-v2 ecwid-SingleProduct-v2-bordered ecwid-SingleProduct-v2-centered ecwid-Product ecwid-Product-150581083" itemscope itemtype="http://schema.org/Product" data-single-product-id="150581083">
+                    <div itemprop="image"></div>
+                    <div class="ecwid-title" itemprop="name" content="Free"></div>
+                    <div itemtype="http://schema.org/Offer" itemscope itemprop="offers">
+                        <div class="ecwid-productBrowser-price ecwid-price" itemprop="price" content="0" data-spw-price-location="button">
+                            <div itemprop="priceCurrency" content="USD"></div>
+                        </div>
+                    </div>
+                    <div customprop="options"></div>
+                    <div customprop="addtobag"></div>
+                </div>
+                <script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?18447751&data_platform=singleproduct_v2" charset="utf-8"></script><script type="text/javascript">xProduct()</script> 
+            </div>
+        </div>
+    </div>
 </section>
 <!--Java Script--> 
 <script
