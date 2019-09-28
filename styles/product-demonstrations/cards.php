309722bbd2036f9003f0f071ac0b5283df4f832a
Andy Knudsen <andy@websitetalkingheads.com>
2019-09-27 18:49:45 -0600

    worked on cards page re-ordered cards

--- styles/product-demonstrations/cards.php
+++ styles/product-demonstrations/cards.php
@@ -12,137 +12,172 @@
 </head>
 
 <body>
-
-<section class="container">
-    <h1 class="text-capitalize text-center">Shots</h1> 
-<div class="card-columns shots">
-  <div class="card"> <img class="card-img-top" src="images/Opening Hero.jpg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Opening Hero</h5>
-      <p class="card-text">Product on right of screen with Logo and Subtitle on Left.</p>
+<section class="container-fluid">
+    <h1 class="text-capitalize text-center">Shots</h1>
+    <div class="container">
+        <h3 class="text-center">Hero Shots</h3>
+        <div class="card-group shots">
+            <div class="card"> <img class="card-img-top" src="images/Opening Hero.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Opening Hero</h5>
+                    <p class="card-text">Product on right of screen with Logo and Subtitle on Left.</p>
+                </div>
+                <div class="card-footer text-right">
+                    <div class="btn btn-custom">Add to Cart</div>
+                </div>
+            </div>
+            <div class="card"> <img class="card-img-top" src="images/Closing Hero.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Closing Hero</h5>
+                    <p class="card-text">Product on left of screen with text on the right. Can include Logo, Subtitle, Call To Action, Website URL</p>
+                </div>
+                <div class="card-footer text-right">
+                    <div class="btn btn-custom">Add to Cart</div>
+                </div>
+            </div>
+            <div class="card"> <img class="card-img-top" src="images/Hero Center (Spinning).jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Hero Center (Spinning)</h5>
+                    <p class="card-text">Product center spinning on a platform</p>
+                </div>
+                <div class="card-footer text-right">
+                    <div class="btn btn-custom">Add to Cart</div>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="card-footer text-right">
-      <div class="btn btn-custom">Add to Cart</div>
+    <hr>
+    <div class="container">
+        <h3 class="text-center">High Angle Shots</h3>
+        <div class="card-group shots">
+            <div class="card"> <img class="card-img-top" src="images/High Angle Hero Left (Spinning).jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">High Angle Hero Left (Spinning)</h5>
+                    <p class="card-text">High angle shot from left with product spinning</p>
+                </div>
+                <div class="card-footer text-right">
+                    <div class="btn btn-custom">Add to Cart</div>
+                </div>
+            </div>
+            <div class="card"> <img class="card-img-top" src="images/High Angle Hero Right (Spinning).jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">High Angle Hero Right (Spinning)</h5>
+                    <p class="card-text">High angle shot from right with product spinning</p>
+                </div>
+                <div class="card-footer text-right">
+                    <div class="btn btn-custom">Add to Cart</div>
+                </div>
+            </div>
+            <div class="card"> <img class="card-img-top" src="images/High Angle Hero Left.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">High Angle Hero Left</h5>
+                    <p class="card-text">High angle shot from left with product spinning</p>
+                </div>
+                <div class="card-footer text-right">
+                    <div class="btn btn-custom">Add to Cart</div>
+                </div>
+            </div>
+            <div class="card"> <img class="card-img-top" src="images/High Angle Hero Right.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">High Angle Hero Right</h5>
+                    <p class="card-text">High angle shot from right with product spinning</p>
+                </div>
+                <div class="card-footer text-right">
+                    <div class="btn btn-custom">Add to Cart</div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-<div class="card"> <img class="card-img-top" src="images/Closing Hero.jpg" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Closing Hero</h5>
-    <p class="card-text">Product on left of screen with text on the right. Can include Logo, Subtitle, Call To Action, Website URL</p>
-  </div>
-  <div class="card-footer text-right">
-    <div class="btn btn-custom">Add to Cart</div>
-  </div>
-</div>
-<div class="card"> <img class="card-img-top" src="images/Dolly Out.jpg" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Dolly Out</h5>
-    <p class="card-text">Product center with camera moving out</p>
-  </div>
-  <div class="card-footer text-right">
-    <div class="btn btn-custom">Add to Cart</div>
-  </div>
-</div>
-<div class="card"> <img class="card-img-top" src="images/Hero Center (Spinning).jpg" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Hero Center (Spinning)</h5>
-    <p class="card-text">Product center spinning on a platform</p>
-  </div>
-  <div class="card-footer text-right">
-    <div class="btn btn-custom">Add to Cart</div>
-  </div>
-</div>
-<div class="card"> <img class="card-img-top" src="images/Dolly Right with Callouts.jpg" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Dolly Right with Callouts</h5>
-    <p class="card-text">Camera Moves to the right.  Show up to 3 callouts(a bit of text that draws attention to an element of your product.)  With a line from the callout to the relevant portion of the product.</p>
-  </div>
-  <div class="card-footer text-right">
-    <div class="btn btn-custom">Add to Cart</div>
-  </div>
-</div>
-<div class="card"> <img class="card-img-top" src="images/High Angle Hero Left (Spinning).jpg" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">High Angle Hero Left (Spinning)</h5>
-    <p class="card-text">High angle shot from left with product spinning</p>
-  </div>
-  <div class="card-footer text-right">
-    <div class="btn btn-custom">Add to Cart</div>
-  </div>
-</div>
-<div class="card"> <img class="card-img-top" src="images/High Angle Hero Right (Spinning).jpg" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">High Angle Hero Right (Spinning)</h5>
-    <p class="card-text">High angle shot from right with product spinning</p>
-  </div>
-  <div class="card-footer text-right">
-    <div class="btn btn-custom">Add to Cart</div>
-  </div>
-</div>
-<div class="card"> <img class="card-img-top" src="images/High Angle Hero Left.jpg" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">High Angle Hero Left</h5>
-    <p class="card-text">High angle shot from left with product spinning</p>
-  </div>
-  <div class="card-footer text-right">
-    <div class="btn btn-custom">Add to Cart</div>
-  </div>
-</div>
-<div class="card"> <img class="card-img-top" src="images/High Angle Hero Right.jpg" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">High Angle Hero Right</h5>
-    <p class="card-text">High angle shot from right with product spinning</p>
-  </div>
-  <div class="card-footer text-right">
-    <div class="btn btn-custom">Add to Cart</div>
-  </div>
-</div>
-<div class="card"> <img class="card-img-top" src="images/Jib Down Right-Spinning.jpg" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Jib Down Right-Spinning</h5>
-    <p class="card-text">Low angle shot from Right with product spinning</p>
-  </div>
-  <div class="card-footer text-right">
-    <div class="btn btn-custom">Add to Cart</div>
-  </div>
-</div>
-<div class="card"> <img class="card-img-top" src="images/Jib Up Left-Description.jpg" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Jib Down Left-Description</h5>
-    <p class="card-text">Low angle shot from left with 5-6 words Describing Product</p>
-  </div>
-  <div class="card-footer text-right">
-    <div class="btn btn-custom">Add to Cart</div>
-  </div>
-  <div class="card"> <img class="card-img-top" src="images/Jib Down Right-Description.jpg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Jib Down Right-Description</h5>
-      <p class="card-text">High angle shot from Right with 5-6 words Describing Product</p>
+    <hr>
+    <div class="container">
+        <h3 class="text-center">Jib Shots</h3>
+        <div class="card-group shots">
+            <div class="card"> <img class="card-img-top" src="images/Jib Down Right-Spinning.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Jib Down Right-Spinning</h5>
+                    <p class="card-text">Low angle shot from Right with product spinning</p>
+                </div>
+                <div class="card-footer text-right">
+                    <div class="btn btn-custom">Add to Cart</div>
+                </div>
+            </div>
+            <div class="card"> <img class="card-img-top" src="images/Jib Up Left-Description.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Jib Down Left-Description</h5>
+                    <p class="card-text">Low angle shot from left with 5-6 words Describing Product</p>
+                </div>
+                <div class="card-footer text-right">
+                    <div class="btn btn-custom">Add to Cart</div>
+                </div>
+            </div>
+            <div class="card"> <img class="card-img-top" src="images/Jib Down Right-Description.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Jib Down Right-Description</h5>
+                    <p class="card-text">High angle shot from Right with 5-6 words Describing Product</p>
+                </div>
+                <div class="card-footer text-right">
+                    <div class="btn btn-custom">Add to Cart</div>
+                </div>
+            </div>
+            <div class="card"> <img class="card-img-top" src="images/Jib Down Left-Spinning.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Jib Down Left-Spinning</h5>
+                    <p class="card-text">Low angle shot from left with product spinning</p>
+                </div>
+                <div class="card-footer text-right">
+                    <div class="btn btn-custom">Add to Cart</div>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="card-footer text-right">
-      <div class="btn btn-custom">Add to Cart</div>
+    <hr>
+    <div class="container">
+        <h3 class="text-center">Dolly Shots</h3>
+        <div class="card-group shots">
+            <div class="card"> <img class="card-img-top" src="images/Dolly Out.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Dolly Out</h5>
+                    <p class="card-text">Product center with camera moving out</p>
+                </div>
+                <div class="card-footer text-right">
+                    <div class="btn btn-custom">Add to Cart</div>
+                </div>
+            </div>
+            <div class="card"> <img class="card-img-top" src="images/Dolly Right with Callouts.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Dolly Right with Callouts</h5>
+                    <p class="card-text">Camera Moves to the right.  Show up to 3 callouts(a bit of text that draws attention to an element of your product.)  With a line from the callout to the relevant portion of the product.</p>
+                </div>
+                <div class="card-footer text-right">
+                    <div class="btn btn-custom">Add to Cart</div>
+                </div>
+            </div>
+            <div class="card"> <img class="card-img-top" src="images/Dolly Out.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Dolly In</h5>
+                    <p class="card-text">Product center with camera moving in.</p>
+                </div>
+                <div class="card-footer text-right">
+                    <div class="btn btn-custom">Add to Cart</div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-  <div class="card"> <img class="card-img-top" src="images/Jib Down Left-Spinning.jpg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Jib Down Left-Spinning</h5>
-      <p class="card-text">Low angle shot from left with product spinning</p>
+    <hr>
+    <div class="container">
+        <h3 class="text-center">Hand Shots</h3>
+        <div class="card-group shots">
+            <div class="card"> <img class="card-img-top" src="images/Product Demo.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Product Demo</h5>
+                    <p class="card-text">Short (5 seconds or less) demo of product showing hands.</p>
+                </div>
+                <div class="card-footer text-right">
+                    <div class="btn btn-custom">Add to Cart</div>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="card-footer text-right">
-      <div class="btn btn-custom">Add to Cart</div>
-    </div>
-  </div>
-  <div class="card"> <img class="card-img-top" src="images/Product Demo.jpg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Product Demo</h5>
-      <p class="card-text">Short (5 seconds or less) demo of product showing hands.</p>
-    </div>
-    <div class="card-footer text-right">
-      <div class="btn btn-custom">Add to Cart</div>
-    </div>
-  </div>
-</div>
 </section>
-
 </body>
 </html>
