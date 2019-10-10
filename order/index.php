2fd6af39e6b2c4ce18e878b3053e8aeafd891e54
Andy Knudsen <paris_giovanni@yahoo.com>
2019-10-10 13:54:38 -0600

    added order page with tabs

---
+++ order/index.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
+<title>Live-Spokesperson|Order</title>
+<meta name="description" content="Live-Spokesperson|Order">
+<meta name="keywords" content="Spokespeople, Video Spokesperson, Virtual Spokesperson, Website Spokesperson, Web Spokesperson, Spokesmodel, Walk On Actor,Virtual Actor, Person on Website, Online Spokesperson.">
+<?php include("../includes/head.php"); ?>
+</head>
+
+<body>
+<?php include("../includes/nav.php"); ?>
+<section class="alert alert-info mb-3">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 ">
+        <nav>
+          <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist"> <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Website Spokesperson</a> <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Product Demonstration</a> <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Whiteboard</a> <a class="nav-item nav-link" id="nav-about-tab" data-toggle="tab" href="#nav-about" role="tab" aria-controls="nav-about" aria-selected="false">Animation</a> </div>
+        </nav>
+        <div class="tab-content py-3 px-3 px-sm-0" id="nav-tabContent">
+          <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+            <div class="ecwid ecwid-SingleProduct-v2 ecwid-SingleProduct-v2-bordered ecwid-SingleProduct-v2-centered ecwid-Product ecwid-Product-152889173" itemscope itemtype="http://schema.org/Product" data-single-product-id="152889173">
+              <div itemprop="image"></div>
+              <div class="ecwid-title" itemprop="name" content="Website Spokesperson"></div>
+              <div itemtype="http://schema.org/Offer" itemscope itemprop="offers">
+                <div class="ecwid-productBrowser-price ecwid-price" itemprop="price" content="0" data-spw-price-location="button">
+                  <div itemprop="priceCurrency" content="USD"></div>
+                </div>
+              </div>
+              <div customprop="options"></div>
+              <div customprop="addtobag"></div>
+            </div>
+            <script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?18447751&data_platform=singleproduct_v2" charset="utf-8"></script><script type="text/javascript">xProduct()</script></div>
+          <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="ecwid ecwid-SingleProduct-v2 ecwid-SingleProduct-v2-bordered ecwid-SingleProduct-v2-centered ecwid-Product ecwid-Product-150569305" itemscope itemtype="http://schema.org/Product" data-single-product-id="150569305">
+                  <div itemprop="image"></div>
+                  <div class="ecwid-title" itemprop="name" content="Overhead Unboxing"></div>
+                  <div itemtype="http://schema.org/Offer" itemscope itemprop="offers">
+                    <div class="ecwid-productBrowser-price ecwid-price" itemprop="price" content="399" data-spw-price-location="button">
+                      <div itemprop="priceCurrency" content="USD"></div>
+                    </div>
+                  </div>
+                  <div customprop="options"></div>
+                  <div customprop="addtobag"></div>
+                </div>
+                <script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?18447751&data_platform=singleproduct_v2" charset="utf-8"></script><script type="text/javascript">xProduct()</script> 
+              </div>
+              <div class="col-md-6">
+                <div class="ecwid ecwid-SingleProduct-v2 ecwid-SingleProduct-v2-bordered ecwid-SingleProduct-v2-centered ecwid-Product ecwid-Product-150581085" itemscope itemtype="http://schema.org/Product" data-single-product-id="150581085">
+                  <div itemprop="image"></div>
+                  <div class="ecwid-title" itemprop="name" content="Platinum"></div>
+                  <div itemtype="http://schema.org/Offer" itemscope itemprop="offers">
+                    <div class="ecwid-productBrowser-price ecwid-price" itemprop="price" content="899" data-spw-price-location="button">
+                      <div itemprop="priceCurrency" content="USD"></div>
+                    </div>
+                  </div>
+                  <div customprop="options"></div>
+                  <div customprop="addtobag"></div>
+                </div>
+                <script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?18447751&data_platform=singleproduct_v2" charset="utf-8"></script><script type="text/javascript">xProduct()</script> 
+              </div>
+            </div>
+          </div>
+          <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+            <div class="ecwid ecwid-SingleProduct-v2 ecwid-SingleProduct-v2-bordered ecwid-SingleProduct-v2-centered ecwid-Product ecwid-Product-153538423" itemscope itemtype="http://schema.org/Product" data-single-product-id="153538423">
+              <div itemprop="image"></div>
+              <div class="ecwid-title" itemprop="name" content="Whiteboard Video"></div>
+              <div itemtype="http://schema.org/Offer" itemscope itemprop="offers">
+                <div class="ecwid-productBrowser-price ecwid-price" itemprop="price" content="999.99" data-spw-price-location="button">
+                  <div itemprop="priceCurrency" content="USD"></div>
+                </div>
+              </div>
+              <div customprop="options"></div>
+              <div customprop="addtobag"></div>
+            </div>
+            <script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?18447751&data_platform=singleproduct_v2" charset="utf-8"></script><script type="text/javascript">xProduct()</script> 
+          </div>
+          <div class="tab-pane fade" id="nav-about" role="tabpanel" aria-labelledby="nav-about-tab">
+            <div class="ecwid ecwid-SingleProduct-v2 ecwid-SingleProduct-v2-bordered ecwid-SingleProduct-v2-centered ecwid-Product ecwid-Product-153539401" itemscope itemtype="http://schema.org/Product" data-single-product-id="153539401">
+              <div itemprop="image"></div>
+              <div class="ecwid-title" itemprop="name" content="Animation Video"></div>
+              <div itemtype="http://schema.org/Offer" itemscope itemprop="offers">
+                <div class="ecwid-productBrowser-price ecwid-price" itemprop="price" content="999.99" data-spw-price-location="button">
+                  <div itemprop="priceCurrency" content="USD"></div>
+                </div>
+              </div>
+              <div customprop="options"></div>
+              <div customprop="addtobag"></div>
+            </div>
+            <script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?18447751&data_platform=singleproduct_v2" charset="utf-8"></script><script type="text/javascript">xProduct()</script> 
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<?php include("../includes/footer.php"); ?>
+</body>
+</html>
