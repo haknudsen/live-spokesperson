c8c7ec3d622ed46a9504ae587b309eaee1458be6
Andy Knudsen <andy@websitetalkingheads.com>
2019-12-11 19:39:31 -0700

    createed a demo page to show the colors of this theme
    changed info color to #4DBEFF

---
+++ demo.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Live Spokesperson Home</title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="description" content="Searching for a new way to bring life to your website and attract more interest from those who visit it? Get an professional spokesperson from Live-Spokesperson.">
+<meta name="Keywords" content="Video Solutions,Product Videos, Video Examples, Animated Video Production, Online Videos, Great Explainer Video, Importance Of Video Marketing, Video Experts, Custom Video Production Services, Web Video Production, New Animated Video, Website Videos, Web Video Services, Web Videos, Video Production Companies, Internet Video, Homepage Web Video, Web Video Production Strategy, Web Video Production, Web Video Production Company Instructional Video">
+<?php include("includes/head.php"); ?>
+</head>
+
+<body style="overflow-x: hidden">
+<?php include("includes/nav.php"); ?>
+<section class="container-fluid">
+	<div class="row">
+		<div class="col-lg-4">
+			<h2>Text Colors</h2>
+			<ul>
+				<li class="text-muted">.text-muted class</li>
+				<li class="text-primary">.text-muted class</li>
+				<li class="text-success">.text-success class</li>
+				<li class="text-info">.text-info class</li>
+				<li class="text-warning">.text-warning class</li>
+				<li class="text-danger">.text-danger class</li>
+				<li class="text-secondary">.text-secondary class</li>
+				<li class="text-white">.text-white class</li>
+				<li class="text-dark">.text-dark class</li>
+				<li class="text-body">.text-body class</li>
+				<li class="text-light">.text-light class</li>
+			</ul>
+		</div>
+		<div class="col-lg-4">
+			<h2>Background Colors</h2>
+			<ul>
+				<li class="bg-primary">.bg-primary class</li>
+				<li class="bg-success">.bg-success class</li>
+				<li class="bg-info">.bg-info class</li>
+				<li class="bg-warning">.bg-warning class</li>
+				<li class="bg-danger">.bg-danger class</li>
+				<li class="bg-secondary">.bg-secondary class</li>
+				<li class="bg-dark">.bg-dark class</li>
+				<li class="bg-light">.bg-light class</li>
+			</ul>
+		</div>
+		<div class="col-lg-4">
+			<h2>Background Gradients</h2>
+			<div class="p-3 mb-2 bg-gradient-primary text-white">.bg-primary</div>
+			<div class="p-3 mb-2 bg-gradient-secondary text-white">.bg-secondary</div>
+			<div class="p-3 mb-2 bg-gradient-success text-white">.bg-success</div>
+			<div class="p-3 mb-2 bg-gradient-danger text-white">.bg-danger</div>
+			<div class="p-3 mb-2 bg-gradient-warning text-white">.bg-warning</div>
+			<div class="p-3 mb-2 bg-gradient-info text-white">.bg-info</div>
+			<div class="p-3 mb-2 bg-gradient-light text-dark">.bg-light</div>
+			<div class="p-3 mb-2 bg-gradient-dark text-light">.bg-dark</div>
+		</div>
+	</div>
+</section>
+<?php include("includes/footer.php"); ?>
+</body>
+</html>
