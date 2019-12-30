f2d263a2c9243b8be963a74ff247596f5095ef2f
Andy Knudsen <paris_giovanni@yahoo.com>
2019-12-30 16:58:43 -0700

    commmit

--- terms.php
+++ terms.php
@@ -29,41 +29,38 @@
     <li class="list-group-item">The Completed final project will be licensed solely to Client, with a copy retained by Live-Spokesperson&trade; for archive or demonstration purposes.</li>
     <li class="list-group-item">Client will hold the exclusive license to the video which will not expire. License is for one domain specified to Live-Spokesperson&trade; at the time of the
       production of the video. Licenses for use on multiple domains must be approved by Live-Spokesperson&trade; prior to production and may include additional fees.</li>
-    <li class="list-group-item">To insure a quality final product, the client agrees that only scripts entered into the order form will be considered the final script.&nbsp; Any previous or
-      future script(s) sent by email or other manner&nbsp;will be considered rough drafts.&nbsp; Updates to the script must be entered into the order form with the Update field selected. Any
-      changes made by the Client to the script or project after&nbsp;completion of this form&nbsp;may alter the final billing.</li>
-    <li class="list-group-item">Live-Spokesperson&trade; shall exercise sole discretion in the hiring and firing of crew and other production staff. The Live-Spokesperson&trade; insures
+    <li class="list-group-item">To ensure a quality final product, the client agrees that only scripts entered into the order form will be considered the final script.&nbsp; Any previous or
+      future script(s) sent by email or other manner&nbsp; will be considered rough drafts.&nbsp; Updates to the script must be entered into the order form with the Update field selected. Any
+      changes made by the Client to the script or project after&nbsp; completion of this form&nbsp; may alter the final billing.</li>
+    <li class="list-group-item">Live-Spokesperson&trade; shall exercise sole discretion in the hiring and firing of the crew and other production staff. The Live-Spokesperson&trade; insures
       the highest possible standards of production for the specified budget.</li>
     <li class="list-group-item">For various reasons, including but not limited to the safety of our actors, Client agrees that by visiting this website, even if no purchase is made, that all
       actors found on WebsiteTalkngHeads.com and it partner websites must be hired for any current or future acting, modeling or other &quot;talent&quot; jobs through Live-Spokesperson only.
-      Client agrees to provide Live-Spokesperson with &quot;Right of First Refusal&quot; on all work involving any actor presented on this site (including &quot;out of state&quot; work).
-      Client agrees to pay Live-Spokesperson the full price rate of production and actor fees for any project using an actor from Website Talking Heads is not given &quot;Right of First
+      The client agrees to provide Live-Spokesperson with &quot; Right of First Refusal&quot; on all work involving any actor presented on this site (including &quot;out of state&quot; work).
+      Client agrees to pay Live-Spokesperson the full price rate of production and actor fees for any project using an actor from Live-Spokesperson is not given &quot;Right of First
       Refusal.&quot;</li>
     <li class="list-group-item">Live-Spokesperson&trade; warrants that the production shall cost no more than the grand total listed on insertion order without prior written consent of the
       Client. Client reserves the right to increase, not decrease said budget at any time after insertion order approval.</li>
     <li class="list-group-item">Live-Spokesperson&trade; insures to provide a finished version of the Project to the Client within a reasonable time frame</li>
-    <li class="list-group-item">Projects must be paid in full at time of order. No Refunds are available for cancellations after payment has been made. Website Talking Heads will allw a
+    <li class="list-group-item">Projects must be paid in full at time of order. No Refunds are available for cancellations after payment has been made. Live-Spokesperson will allow a
       credit for up to 18 months from the time of original purchase.</li>
     <li class="list-group-item">Client agrees that wardrobe choices are limited to what the actor has available in their personal wardrobe.&nbsp; Specific requests&nbsp; for wardrobe outside
-      of actors wardrobe selection must be&nbsp;paid for&nbsp;by client or purchased by client and mailed to Live-Spokesperson. Upon request only, Live-Spokesperson&nbsp;may send electronic
-      photos to Client at time of shooting for final approval. This must be pre-scheduled and client must be available for the entire day of the shoot. This may cause a delay in the delivery
+      of actors, wardrobe selection must be&nbsp; paid for&nbsp;by client or purchased by the client and mailed to Live-Spokesperson. Upon request only, Live-Spokesperson&nbsp; may send electronic photos to Client at the time of shooting for final approval. This must be pre-scheduled and the client must be available for the entire day of the shoot. This may cause a delay in the delivery
       time of the video(s).</li>
-    <li class="list-group-item">Website Talking Heads actors will perform the scripts as provided. Pronunciations of industry terms or other potentially difficult words must be provided in
-      the order form if client expects them to be correct. Client agrees not to assume we have a knowledge of any terms or acronyms in their industry. Acronyms MUST have instructions of how to
-      say them. Failure to provide specific pronunciations releases Website Talking Heads from being at fault in the final delivery. A reshoot few will apply. any&nbsp;additional needs or
-      directions&nbsp;must be given in the &quot;Additional comments and request&quot; text field. Any direction for pointing, gesturing, tone, inflection or attitude must be provided in the
-      order form if client expects or requires it. If left out, then no specifics are not required. Because tone and inflection are part of the art of acting and there are many ways to deliver
-      a performance, the actor's delivery of the words provided on the script will be considered a completed project.</li>
-    <li class="list-group-item">The WebsiteTalking Heads Order Form will be considered to final decision, all previous commuincations and requests via email will be disregarded.&nbsp;There
-      are no other agreements relative to this Project between either of the parties hereto either verbally or in writing and this document , The form constitutes the entirety of the
+    <li class="list-group-item">Live-Spokesperson actors will perform the scripts as provided. Pronunciations of industry terms or other potentially difficult words must be provided in
+      the order form if the client expects them to be correct. The client agrees not to assume we have a knowledge of any terms or acronyms in their industry. Acronyms MUST have instructions of how to say them. Failure to provide specific pronunciations releases Live-Spokesperson from being at fault in the final delivery. A reshoot few will apply. any&nbsp;additional needs or
+      directions&nbsp;must be given in the &quot;Additional comments and request&quot; text field. Any direction for pointing, gesturing, tone, inflection or attitude must be provided in the order form if the client expects or requires it. If left out, then no specifics are not required. Because tone and inflection are part of the art of acting and there are many ways to deliver
+       performance, the actor's delivery of the words provided on the script will be considered a completed project.</li>
+    <li class="list-group-item">The WebsiteTalking Heads Order Form will be considered to the final decision, all previous communications and requests via email will be disregarded.&nbsp;There
+      are no other agreements relative to this Project between either of the parties hereto either verbally or in writing and this document, The form constitutes the entirety of the
       contract.</li>
-    <li class="list-group-item">A minimum &ldquo;Reshoot Fee&rdquo; of $150 per video (longer videos may be more) will be assessed for any reshoot done because of innaccurate, missing, or
+    <li class="list-group-item">A minimum &ldquo;Reshoot Fee&rdquo; of $150 per video (longer videos may be more) will be assessed for any reshoot done because of inaccurate, missing, or
       unclear&nbsp;information in script or request.&nbsp;</li>
     <li class="list-group-item">The Client is solely responsible for the content of the script(s).&nbsp; Any claims or statements made in said script(s) are the responsibility of the
       Client.&nbsp; Live-Spokesperson&trade; reserves the right to refuse scripts for any content it deems inappropriate or unacceptable. Live-Spokesperson&trade; reserves the right to
       request documentation backing up any claims made.&nbsp;</li>
     <li class="list-group-item">I have read, understand and agree with every provision of this contract&nbsp;selecting the &quot;I Agree to the Terms and Conditions&quot; radio button, hereby
-      certify that I accept and agree to abide by them and that I am competent and legally qualified to enter in to such a contract.</li>
+      certify that I accept and agree to abide by them and that I am competent and legally qualified to enter into such a contract.</li>
   </ul>
 </section>
 <?php include("includes/footer.php"); ?>
