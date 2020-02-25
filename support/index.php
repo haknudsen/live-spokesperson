d87230c9fa71bb3563d6de6a9cab0cdef0cdf5c5
Andy Knudsen <paris_giovanni@yahoo.com>
2020-02-24 17:39:29 -0700

    Added Support FAQ

---
+++ support/index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Support Website Talking Heads&reg;</title>
+  <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1">
+  <meta name="keywords" content="support, player support, online spokesperson, video spokesperson, website talking heads,">
+  <meta name="description" content="Online video spokesperson support. ">
+  <meta name="robots" content="index, follow">
+  <!-- (Robot commands: All, None, Index, No Index, Follow, No Follow) -->
+  <meta name="revisit-after" content="30 days">
+  <meta name="distribution" content="global">
+  <meta name="rating" content="general">
+  <meta name="Content-Language" content="english">
+  <meta name="verify-v1" content="YNESpqoAwK51PmBV7/PFKLG0agx7AQPKhXXcYAXGGF8="/>
+  <meta name="norton-safeweb-site-verification" content="iinbv24r-1ix20hgj5l94wz2rnn3aiwi0336hwysvvpiskquy6ijsh9wy12f3znbed-hz1ay8ppzhgqap-sicqtw6ui29d0wrfcpenudh1ml9xwjbej7u25xy9pnm6yr"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php include("../includes/head.php"); ?>
+</head>
+
+<body sclass="cs-HTML theme-unified">
+<?php include("../includes/nav.php"); ?>
+<section class="container-fluid mb-2">
+  <h1 class="text-center">Support FAQ and Contact</h1>
+	<div class="faq align-content-center">
+	<iframe frameBorder="0" src="story.html"></iframe>
+	</div>
+</section>
+<?php include("../includes/footer.php"); ?>
+</body>
+</html>
