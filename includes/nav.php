8ee65a504a67fba6e21aee560ec5c7d8a1a12531
haknudsen <paris_giovanni@yahoo.com>
2020-07-21 16:50:31 -0600

    added motion design style

--- includes/nav.php
+++ includes/nav.php
@@ -1,18 +1,19 @@
 <div class="se-pre-con"></div>
-<nav class="navbar navbar-expand-xl navbar-dark bg-gradient-dark sticky-top animated fadeIn"> <a href="http://www.live-spokesperson.com/" title="Live-Spokesperson.com" class="navbar-brand float-left"><img src="https://www.websitetalkingheads.com/images/Talking_Heads_Banner_Logo.png" alt="Live-Spokesperson Logo" class="img-fluid" style="max-height: 98px" title="Live-Spokesperson Logo"/></a>
+  <nav class="navbar navbar-expand-xl navbar-dark bg-gradient-dark sticky-top animated fadeIn">
+    <a href="http://www.live-spokesperson.com/" title="Live-Spokesperson.com" class="navbar-brand float-left"><img src="https://www.websitetalkingheads.com/images/Talking_Heads_Banner_Logo.png" alt="Live-Spokesperson Logo" class="img-fluid" style="max-height: 98px" title="Live-Spokesperson Logo"/></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
     <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-        <ul class="navbar-nav">
-            <li class="nav-item" id="home"> <a class="nav-link" href="http://www.live-spokesperson.com/">Home</a> </li>
-            <li class="nav-item" id="spokespeople"> <a class="nav-link" href="http://www.live-spokesperson.com/spokespeople/women.php">Spokespeople</a> </li>
-            <li class="nav-item dropdown" id="styles"> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Styles</a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown"> <a href="http://www.live-spokesperson.com/styles/" title="Styles" class="dropdown-item">Styles</a> <a href="#" title="Website Spokesperson" class="dropdown-item">Website Spokesperson</a>
-                    <div class="dropdown-divider"></div>
-                    <a href="http://www.live-spokesperson.com/styles/whiteboard/" title="Whiteboard" class="dropdown-item">Whiteboard</a> <a href="http://www.live-spokesperson.com/styles/animation" title="Animation" class="dropdown-item">Animation</a> <a href="http://www.live-spokesperson.com/styles/Typography" title="Typography" class="dropdown-item">Typography</a> <a href="http://www.live-spokesperson.com/styles/custom-presentations/" title="Presentation" class="dropdown-item">Presentation</a> <a href="http://www.live-spokesperson.com/styles/product-demonstrations/" title="Product Demo" class="dropdown-item">Product Demo</a> </div>
-            </li>
-            <li class="nav-item" id="order"> <a class="nav-link" href="http://www.live-spokesperson.com/order">Order</a> </li>
-            <li class="nav-item" id="specials"> <a class="nav-link" href="http://www.live-spokesperson.com/specials">Specials</a> </li>
-        </ul>
+      <ul class="navbar-nav">
+        <li class="nav-item" id="home"> <a class="nav-link" href="http://www.live-spokesperson.com/">Home</a> </li>
+        <li class="nav-item" id="spokespeople"> <a class="nav-link" href="http://www.live-spokesperson.com/spokespeople/women.php">Spokespeople</a> </li>
+        <li class="nav-item dropdown" id="styles"> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Styles</a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown"> <a href="http://www.live-spokesperson.com/styles/" title="Styles" class="dropdown-item">Styles</a> <a href="#" title="Website Spokesperson" class="dropdown-item">Website Spokesperson</a>
+            <div class="dropdown-divider"></div>
+            <a href="http://www.live-spokesperson.com/styles/whiteboard/" title="Whiteboard" class="dropdown-item">Whiteboard</a> <a href="http://www.live-spokesperson.com/styles/animation" title="Animation" class="dropdown-item">Animation</a> <a href="http://www.live-spokesperson.com/styles/Typography" title="Typography" class="dropdown-item">Typography</a> <a href="http://www.live-spokesperson.com/styles/custom-presentations/" title="Presentation" class="dropdown-item">Presentation</a> <a href="http://www.live-spokesperson.com/styles/product-demonstrations/" title="Product Demo" class="dropdown-item">Product Demo</a><a href="http://www.live-spokesperson.com/styles/motion-design/" title="Motion Design" class="dropdown-item">Motion Design</a> </div>
+        </li>
+        <li class="nav-item" id="order"> <a class="nav-link" href="http://www.live-spokesperson.com/order">Order</a> </li>
+        <li class="nav-item" id="specials"> <a class="nav-link" href="http://www.live-spokesperson.com/specials">Specials</a> </li>
+      </ul>
     </div>
     <a href="tel:1-801-748-2281" title="Call Now!" class="navbar-brand float-right">1-801-748-2281</a>
-</nav>
+  </nav>
