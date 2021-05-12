6d941675e54c31e3be69e450213249ef6c69b8e3
Andy Knudsen <andy@websitetalkingheads.com>
2021-05-12 12:41:16 -0600

    starting to work on new database

---
+++ database/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<title>Custom Video Styles by Talking Heads®</title>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<meta content="Custom Video,Website Video,Video Presentation,Animation,Custom Illustration,Whiteboard Video,Explainer Video" name="keywords">
+<meta content="Talking Heads Video creates a wide variety of videos.  From a simple and quick message to Product Demonstrations, Website Walk-throughs, Training, or Sales. " name="description">
+<meta content="index, follow" name="robots">
+<!-- (Robot commands: All, None, Index, No Index, Follow, No Follow) -->
+<meta content="30 days" name="revisit-after">
+<meta content="global" name="distribution">
+<meta content="general" name="rating">
+<meta content="english" name="Content-Language">
+<meta content="YNESpqoAwK51PmBV7/PFKLG0agx7AQPKhXXcYAXGGF8=" name="verify-v1">
+<meta content="iinbv24r-1ix20hgj5l94wz2rnn3aiwi0336hwysvvpiskquy6ijsh9wy12f3znbed-hz1ay8ppzhgqap-sicqtw6ui29d0wrfcpenudh1ml9xwjbej7u25xy9pnm6yr" name="norton-safeweb-site-verification">
+<meta content="width=device-width, initial-scale=1" name="viewport">
+<?php include("../includes/head.php"); ?>
+</head>
+
+<body>
+<?php include("../includes/nav.php"); ?>
+<section class="alert alert-styles">
+  <div class="container">
+    <?php $type = "Presentation";$show = 6;$rand = false;$columns = 3; include("../includes/showDemo.php");?>
+  </div>
+</section>
+<?php include("../includes/footer.php"); ?>
+<?php include("../includes/modal.php");?>
+</body>
+</html>
