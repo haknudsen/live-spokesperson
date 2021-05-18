3b3c61fbc954a62fcc9808473ef1a6d5eb38ec89
Andy Knudsen <andy@websitetalkingheads.com>
2021-05-18 13:40:09 -0600

    working on template

--- database/includes/showStyle.php
+++ database/includes/showStyle.php
@@ -6,7 +6,7 @@ if(isset($style)){
     $style = "whiteboard";
 }
 $sql = "SELECT * FROM ". $style ."_content ORDER BY RAND() LIMIT 1";
-echo ($sql . "<br>");
+//echo ($sql . "<br>");
 require_once ("connect-aws.php");
 $result = $con->query($sql);
 if ($result->num_rows > 0)
@@ -15,9 +15,7 @@ if ($result->num_rows > 0)
     echo '<div class="container">';
     while ($row = $result->fetch_assoc())
     {
-        $name = $row["name"];
-        $content = $row["content"];
-    echo '<h2 class="text-center">' . $row[ "name" ] . '</h2>
+    echo '<h2 class="text-center">' . $row[ "title" ] . '</h2>
         <div class="text">'.$row["content"].'</div>
         </div>';
     }
