c9eca4af03fa382c9080405064f21e302e6eb644
Andy Knudsen <andy@websitetalkingheads.com>
2021-05-19 17:58:56 -0600

    finished up for now

--- spokespeople/index.php
+++ spokespeople/index.php
@@ -12,37 +12,29 @@
 
 <body>
 <?php include("../includes/nav-spokesperson.php"); ?>
-    <section class="alert alert-info mb-3">
-      <div class="container">
-        <h2 class="text-center">Our Spokespeople</h2>
-        <div class="w-50 float-left pr-1 fadeInLeft">
-          <div class="embed-responsive embed-responsive-16by9">
-            <iframe class="embed-responsive-item" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../ivideo/talking-heads-player.php?video=Massive Wall Of Spokespeople&autostart=mouse"></iframe>
-          </div>
-        </div>
-        <div class="wrapped-text">
-          <p>People call them Video Spokesperson, Virtual Spokesperson, Web Spokesperson, Spokes-Model, Walk On Actor, Virtual Actor, Person On Website, Online Spokesperson, Web Presenter, Video Presenter, Website Presenter, Video Web Presenter, Promo Video Presenter, Live Actor, Website Video Actor, Virtual Live Actor, iSpokesperson and more...</p>
-          <p>They are the same as as the presenters you see on Television. They communicate suggestions, news, updates, or attempting to sell. They will keep visitors' attention so they stay longer on your website. Not a simple task considering the brief attention period that many people have! That is why picking the most suitable spokesperson with the job is very important.</p>
-        </div>
+<section class="alert alert-info mb-3">
+  <div class="container">
+    <h2 class="text-center">Our Spokespeople</h2>
+    <div class="w-50 float-left pr-1 fadeInLeft">
+      <div class="embed-responsive embed-responsive-16by9">
+        <iframe class="embed-responsive-item" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../ivideo/talking-heads-player.php?video=Massive Wall Of Spokespeople&autostart=mouse"></iframe>
       </div>
-    </section>
-    <section class="container-fluid bg-white">
-      <div class="container">
-        <div class="card middle">
-          <h3 class="card-header bg-gradient-mine text-center text-white">Video <em>Spokespeople</em></h3>
-          <div class="card-img-top" style="max-width: 640px;margin: 0 auto">
-            <div class="embed-responsive embed-responsive-16by9">
-              <iframe class="embed-responsive-item" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="../ivideo/talking-heads-player.php?video=Professional Spokesperson&autostart=no"></iframe>
-            </div>
-          </div>
-          <div class="card-body">
-            <h5 class="card-title text-capitalize">You Have Seen Them Before</h5>
-            <p class="card-text">They are the people who welcome you to a website. Your video spokesperson, also know as virtual actor or website spokesperson, is a transparent video shown over a website. Instead of reading, which can get dull, these spokesperson typically walk you through whatever you need to know about the page</p>
-          </div>
-          <div class="card-footer text-muted bg-gradient-mine text-right"><a href="../spokespeople/" title="Spokespeople"><em class="text-white wow flash">More...</em></div>
-        </div>
-      </div>
-    </section>
-    <?php include("../includes/footer-spokespeople.php"); ?>
+    </div>
+    <div class="wrapped-text">
+      <p>People call them Video Spokesperson, Virtual Spokesperson, Web Spokesperson, Spokes-Model, Walk On Actor, Virtual Actor, Person On Website, Online Spokesperson, Web Presenter, Video Presenter, Website Presenter, Video Web Presenter, Promo Video Presenter, Live Actor, Website Video Actor, Virtual Live Actor, iSpokesperson and more...</p>
+      <p>They are the same as as the presenters you see on Television. They communicate suggestions, news, updates, or attempting to sell. They will keep visitors' attention so they stay longer on your website. Not a simple task considering the brief attention period that many people have! That is why picking the most suitable spokesperson with the job is very important.</p>
+    </div>
+  </div>
+</section>
+<section class="container-fluid bg-white animated rubberBand">
+  <?php
+    $style = "spokesperson";
+    include ('../database/includes/showStyle.php'); 
+    ?>
+</section>
+<section class="alert alert-info my-3 animated backInDown">
+  <?php include ('../database/includes/showCard.php'); ?>
+</section>
+<?php include("../includes/footer-spokespeople.php"); ?>
 </body>
 </html>
